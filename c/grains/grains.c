1615d647245d136a018d0e0d3ef8135e9fe22947
Jiří Martínek <zwo@centrum.cz>
2022-11-14 16:41:02 +0100

    c_difference-of-squares started.

--- c/grains/grains.c
+++ c/grains/grains.c
@@ -1,1 +1,32 @@
 #include "grains.h"
+
+uint64_t square(uint8_t index)
+{
+    uint64_t    result;
+
+    result = 1;
+    if (index < 1 || index > 64)
+    {
+        return (0);
+    }
+    while (index > 1)
+    {
+        result *= 2;
+        index--;
+    }
+    return (result);
+}
+uint64_t total(void)
+{
+    uint64_t    result;
+    int         i;
+
+    result = 0;
+    i = 1;
+    while (i <= 64)
+    {
+        result += square(i);
+        i++;
+    }
+    return (result);
+}
