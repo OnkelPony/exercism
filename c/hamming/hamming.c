4ad24b27179449946d3d9370c71762ace1ef9dce
Jiří Martínek <zwo@centrum.cz>
2022-11-14 13:58:25 +0100

    c_hamming complete

--- c/hamming/hamming.c
+++ c/hamming/hamming.c
@@ -1,1 +1,23 @@
 #include "hamming.h"
+#include <string.h>
+
+int compute(const char *lhs, const char *rhs)
+{
+	int	result;
+
+	result = 0;
+	if (strlen(lhs) != strlen(rhs))
+	{
+		return (-1);
+	}
+	while (*lhs)
+	{
+		if (*lhs != *rhs)
+		{
+			result++;
+		}
+		lhs++;
+		rhs++;
+	}
+	return (result);
+}
