b17ea40f2dd19e776efde5d1ae22bad4f3547fe1
Jiří Martínek <zwo@centrum.cz>
2022-11-15 15:32:03 +0100

    c_square_root complete.

--- c/square-root/square_root.c
+++ c/square-root/square_root.c
@@ -1,1 +1,24 @@
 #include "square_root.h"
+
+unsigned int	square_root(unsigned int number)
+{
+	unsigned int	c;
+	unsigned int	d;
+
+	c = 0;
+	d = 1 << 30;
+	while (d > number)
+		d >>= 2;
+	while (d != 0) {
+		if (number >= c + d)
+		{
+			number -= c + d;
+			c = (c >> 1) + d;
+		}
+		else {
+			c >>= 1;
+		}
+		d >>= 2;
+	}
+	return (c);
+}
