a5390002c88a84bc2cf1829140c0d9a8972116d2
OnkelPony <zkurvenej@outlook.com>
2022-11-27 00:06:25 +0100

    grade_school started.

--- c/grade-school/grade_school.c
+++ c/grade-school/grade_school.c
@@ -1,1 +1,23 @@
 #include "grade_school.h"
+
+void init_roster(roster_t *roster)
+{
+	roster->count = 0;
+}
+
+bool add_student(roster_t *roster, char *name, int grade)
+{
+	bool succeed;
+
+	roster->count = 0;
+	strcpy(roster.students[0].name, name);
+	succeed = true;
+	return succeed;
+}
+
+roster_t get_grade(roster_t *roster, int grade)
+{
+	roster_t result;
+
+	return result;
+}
