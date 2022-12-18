e84be3069987f184d4c784e84869e7a0e5119ced
OnkelPony <zkurvenej@outlook.com>
2022-12-18 23:11:04 +0100

    c_circular-buffer progress_002

--- c/circular-buffer/circular_buffer.c
+++ c/circular-buffer/circular_buffer.c
@@ -1,1 +1,16 @@
 #include "circular_buffer.h"
+
+int16_t read(circular_buffer_t *buffer, buffer_value_t *read_value);
+
+circular_buffer_t *new_circular_buffer(size_t capacity);
+
+void delete_buffer(circular_buffer_t *buffer);
+
+void clear_buffer(circular_buffer_t *buffer);
+
+int16_t write(circular_buffer_t *buffer, buffer_value_t value);
+
+int16_t overwrite(circular_buffer_t *buffer, buffer_value_t value)
+{
+	
+}
--- c/circular-buffer/circular_buffer.h
+++ c/circular-buffer/circular_buffer.h
@@ -1,4 +1,18 @@
 #ifndef CIRCULAR_BUFFER_H
 #define CIRCULAR_BUFFER_H
+#include <stdint.h>
+#include <stddef.h>
+
 typedef int buffer_value_t;
+typedef struct circular_buffer_t
+{
+	size_t capacity;
+	
+} circular_buffer_t;
+int16_t read(circular_buffer_t *buffer, buffer_value_t *read_value);
+circular_buffer_t *new_circular_buffer(size_t capacity);
+void delete_buffer(circular_buffer_t *buffer);
+void clear_buffer(circular_buffer_t *buffer);
+int16_t write(circular_buffer_t *buffer, buffer_value_t value);
+int16_t overwrite(circular_buffer_t *buffer, buffer_value_t value);
 #endif
