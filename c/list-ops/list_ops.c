6ce8a6e4b830a931be9fcf65479a80cc34f03fd4
Jiří Martínek <zwo@centrum.cz>
2022-12-19 19:00:33 +0100

    c_list-ops 17 tests remaining.

--- c/list-ops/list_ops.c
+++ c/list-ops/list_ops.c
@@ -4,13 +4,28 @@ list_t *new_list(size_t length, list_element_t elements[])
 {
 	list_t *p_list;
 
-	p_list = malloc(sizeof(*p_list) * length);
-	p_list->elements = elements;
-	p_list->length = length;
+	p_list = malloc(sizeof(*p_list));
+	// p_list->elements = elements;
+	// p_list->length = length;
+	if (elements)
+	{
+		elements[0] = length;
+	}
 	return (p_list);
 }
 
 void delete_list(list_t *list)
 {
 	free(list);
+}
+
+list_t *append_list(list_t *list1, list_t *list2)
+{
+	list_t *p_list;
+	// size_t length;
+
+	p_list = malloc(sizeof(*p_list));
+	list1->length = list2->length;
+	// p_list->length = length;
+	return (p_list);
 }
