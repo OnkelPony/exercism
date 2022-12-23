0198762b544d59bb5f4603a41cee6b8c10028180
Jiří Martínek <zwo@centrum.cz>
2022-12-23 15:24:06 +0100

    c_circular-buffer 12 tests remaining.

--- c/circular-buffer/circular_buffer.c
+++ c/circular-buffer/circular_buffer.c
@@ -1,13 +1,27 @@
 #include <stdlib.h>
+#include <errno.h>
 #include "circular_buffer.h"
 
-int16_t read(circular_buffer_t *buffer, buffer_value_t *read_value);
+int16_t read(circular_buffer_t *buffer, buffer_value_t *read_value)
+{
+	*read_value = buffer->values[buffer->start];
+	if (buffer->length == 0)
+	{
+		errno = ENODATA;
+		return (EXIT_FAILURE);
+	}
+	return (EXIT_SUCCESS);
+}
 
 circular_buffer_t *new_circular_buffer(size_t capacity)
 {
 	circular_buffer_t *p_buffer;
 
 	p_buffer = malloc(sizeof(*p_buffer) + capacity * sizeof(p_buffer->values[0]));
+	p_buffer->capacity = capacity;
+	p_buffer->start = 0;
+	p_buffer->end = 0;
+	p_buffer->length = 0;
 	return (p_buffer);
 }
 
@@ -18,10 +32,16 @@ void delete_buffer(circular_buffer_t *buffer)
 
 void clear_buffer(circular_buffer_t *buffer);
 
-int16_t write(circular_buffer_t *buffer, buffer_value_t value);
+int16_t write(circular_buffer_t *buffer, buffer_value_t value)
+{
+	buffer->values[buffer->end] = value;
+	buffer->end++;
+	buffer->length++;
+	return (EXIT_SUCCESS);
+}
 
 int16_t overwrite(circular_buffer_t *buffer, buffer_value_t value)
 {
-	buffer->end = value;
-	return(EXIT_SUCCESS);
+	buffer->values[buffer->end] = value;
+	return (EXIT_SUCCESS);
 }
