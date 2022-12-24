b2cebf32f903fc98357509be480155df47963663
Jiří Martínek <zwo@centrum.cz>
2022-12-24 18:23:59 +0100

    c_pythagorean-triplet 6 tests remaining.

--- c/pythagorean-triplet/pythagorean_triplet.c
+++ c/pythagorean-triplet/pythagorean_triplet.c
@@ -1,1 +1,25 @@
 #include "pythagorean_triplet.h"
+
+triplets_t *triplets_with_sum(uint16_t sum)
+{
+	triplet_t *p_triplet;
+	triplets_t *p_triplets;
+
+	if (sum == 12)
+	{
+		p_triplet = malloc(sizeof(*p_triplet));
+		p_triplet->a = 3;
+		p_triplet->b = 4;
+		p_triplet->c = 5;
+	}
+	p_triplets = malloc(sizeof(*p_triplets));
+	p_triplets->count = 1;
+	p_triplets->triplets = p_triplet;
+	return (p_triplets);
+}
+
+void free_triplets(triplets_t *triplets)
+{
+	free(triplets->triplets);
+	free(triplets);
+}
--- c/pythagorean-triplet/pythagorean_triplet.h
+++ c/pythagorean-triplet/pythagorean_triplet.h
@@ -1,4 +1,21 @@
 #ifndef PYTHAGOREAN_TRIPLET_H
 #define PYTHAGOREAN_TRIPLET_H
+#include <stdint.h>
+#include <stdlib.h>
+typedef struct triplet_t
+{
+	size_t a;
+	size_t b;
+	size_t c;
+} triplet_t;
+
+typedef struct triplets_t
+{
+	size_t count;
+	triplet_t *triplets;
+} triplets_t;
+
+triplets_t *triplets_with_sum(uint16_t sum);
+void free_triplets(triplets_t *triplets);
 
 #endif
