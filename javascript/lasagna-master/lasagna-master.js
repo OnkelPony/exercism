52e0d7c046d664347ffc8acab683a68fa5e80673
Jiří Martínek <zkurvenej@outlook.com>
2023-03-13 17:55:02 +0100

    js_lasagna-master preparationTime complete.

--- javascript/lasagna-master/lasagna-master.js
+++ javascript/lasagna-master/lasagna-master.js
@@ -10,3 +10,30 @@
  *   ...
  * }
  */
+
+/**
+ * Determins the status of the lasagna.
+ *
+ * @param {number} minutesRemaining
+ * @return {string} lasagna status
+ */
+export function cookingStatus(minutesRemaining) {
+	switch (minutesRemaining) {
+		case 0:
+			return "Lasagna is done.";
+		case undefined:
+			return "You forgot to set the timer.";
+		default:
+			return "Not done, please wait.";
+	}
+}
+
+/**
+ * Estimate the preparation time.
+ * @param {string[]} layers
+ * @param {number} minutesPerLayer
+ * @return {number} preparation time
+ */
+export function preparationTime(layers, minutesPerLayer = 2) {
+	return minutesPerLayer * (layers.length);
+}
