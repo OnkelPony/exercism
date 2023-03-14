a8416761bd2d8477dee0815e0991866240cfd7ae
Jiří Martínek <zkurvenej@outlook.com>
2023-03-14 18:57:31 +0100

    js_custom-signs buildBirthdaySign complete.

--- javascript/custom-signs/custom-signs.js
+++ javascript/custom-signs/custom-signs.js
@@ -14,7 +14,7 @@
  */
 
 export function buildSign(occasion, name) {
-  throw new Error('Implement the buildSign function');
+  return `Happy ${occasion} ${name}!`;
 }
 
 /**
@@ -26,7 +26,7 @@ export function buildSign(occasion, name) {
  */
 
 export function buildBirthdaySign(age) {
-  throw new Error('Implement the buildBirthdaySign function');
+  return `Happy Birthday! What a ${age >= 50 ? "mature" : "young"} fellow you are.`;
 }
 
 /**
