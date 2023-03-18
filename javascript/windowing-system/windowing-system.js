f365fa955964f2317e1e9951a8e8c0d93c34e0b8
Jiří Martínek <zkurvenej@outlook.com>
2023-03-18 18:21:02 +0100

    js_windowing-system complete.

--- javascript/windowing-system/windowing-system.js
+++ javascript/windowing-system/windowing-system.js
@@ -50,9 +50,64 @@ Position.prototype.move = function (newX, newY) {
  * Define a ProgramWindow class.
  */
 export class ProgramWindow {
-	constructor () {
+	constructor() {
 		this.screenSize = new Size(800, 600);
 		this.size = new Size();
 		this.position = new Position();
 	}
+
+	resize(newSize) {
+		let maxWidth = this.screenSize.width - this.position.x;
+		let maxHeight = this.screenSize.height - this.position.y;
+		this.size = newSize;
+		if (newSize.width > maxWidth) {
+			this.size.width = maxWidth;
+		}
+		if (newSize.height > maxHeight) {
+			this.size.height = maxHeight;
+		}
+		if (newSize.width < 1) {
+			this.size.width = 1;
+		}
+		if (newSize.height < 1) {
+			this.size.height = 1;
+		}
+	}
+
+	move(newPosition) {
+		let maxX = this.screenSize.width - this.size.width;
+		let maxY = this.screenSize.height - this.size.height;
+		this.position = newPosition;
+		if (newPosition.x > maxX) {
+			this.position.x = maxX;
+		}
+		if (newPosition.y > maxY) {
+			this.position.y = maxY;
+		}
+		if (newPosition.x < 0) {
+			this.position.x = 0;
+		}
+		if (newPosition.y < 0) {
+			this.position.y = 0;
+		}
+	}
+}
+
+/**
+ * Implement a changeWindow function that accepts a ProgramWindow instance
+ * as input and changes the window to the specified size and position.
+ * The function should return the ProgramWindow instance
+ * that was passed in after the changes were applied.
+ * The window should get a width of 400, a height of 300
+ * and be positioned at x = 100, y = 150.
+ *
+ * @param {ProgramWindow} programWindow
+ * @returns {ProgramWindow} passed instance after the changes were applied
+ */
+export function changeWindow(programWindow) {
+	const newPosition = new Position(100, 150);
+	const newSize = new Size(400, 300);
+	programWindow.move(newPosition);
+	programWindow.resize(newSize);
+	return programWindow;
 }
