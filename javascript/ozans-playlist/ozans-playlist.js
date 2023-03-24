a0c3933115f9de36222f2bd4c2afc791fd1ff4ef
OnkelPony <zkurvenej@outlook.com>
2023-03-24 20:34:45 +0100

    js_ozans-playlist complete.

--- javascript/ozans-playlist/ozans-playlist.js
+++ javascript/ozans-playlist/ozans-playlist.js
@@ -11,7 +11,7 @@
  * @returns {string[]} new playlist with unique entries
  */
 export function removeDuplicates(playlist) {
-  throw new Error('Please implement the removeDuplicates function');
+  return [...new Set(playlist)];
 }
 
 /**
@@ -22,7 +22,8 @@ export function removeDuplicates(playlist) {
  * @returns {boolean} whether the track is in the playlist
  */
 export function hasTrack(playlist, track) {
-  throw new Error('Please implement the hasTrack function');
+  const noDupsPlaylist = new Set(playlist);
+  return noDupsPlaylist.has(track);
 }
 
 /**
@@ -33,7 +34,9 @@ export function hasTrack(playlist, track) {
  * @returns {string[]} new playlist
  */
 export function addTrack(playlist, track) {
-  throw new Error('Please implement the addTrack function');
+  const noDupsPlaylist = new Set(playlist);
+  noDupsPlaylist.add(track);
+  return [...noDupsPlaylist];
 }
 
 /**
@@ -44,7 +47,9 @@ export function addTrack(playlist, track) {
  * @returns {string[]} new playlist
  */
 export function deleteTrack(playlist, track) {
-  throw new Error('Please implement the deleteTrack function');
+  const noDupsPlaylist = new Set(playlist);
+  noDupsPlaylist.delete(track);
+  return [...noDupsPlaylist];
 }
 
 /**
@@ -54,5 +59,9 @@ export function deleteTrack(playlist, track) {
  * @returns {string[]} list of artists
  */
 export function listArtists(playlist) {
-  throw new Error('Please implement the listArtists function');
+  const artists = new Set();
+  playlist.forEach(item => {
+    artists.add(item.split(" - ")[1]);
+  });
+  return [...artists];
 }
