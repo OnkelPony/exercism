7110698008194ecbfe6b4a65f8468374f7d0ab80
OnkelPony <zkurvenej@outlook.com>
2023-04-18 22:49:42 +0200

    js_matrix complete.

--- javascript/matrix/matrix.js
+++ javascript/matrix/matrix.js
@@ -1,20 +1,18 @@
-//
-// This is only a SKELETON file for the 'Matrix' exercise. It's been provided as a
-// convenience to get you started writing code faster.
-//
-
 export class Matrix {
     constructor(numberString) {
-        this.result = [];
-        this.matrixRows = numberString.split("\n");
-        this.matrixRows.forEach(row => this.result.push(row.split(" ").map(element => Number(element))));
+        this.result = numberString.split("\n")
+            .map(line => line.split(" ")
+                .map(Number));
     }
 
+    static transMatrix(rows) {
+        return rows[0].map((_, i) => rows.map(row => row[i]));
+    }
     get rows() {
         return this.result;
     }
 
     get columns() {
-        return this.result[0].map((_, i) => this.result.map(row => row[i]));
+        return Matrix.transMatrix(this.result);
     }
 }
