ffa8ab99546f367d83a5363779d28fa0b31ab34b
OnkelPony <zkurvenej@outlook.com>
2023-05-23 22:45:27 +0200

    cs_phone-number-analysis complete.

--- csharp/phone-number-analysis/PhoneNumberAnalysis.cs
+++ csharp/phone-number-analysis/PhoneNumberAnalysis.cs
@@ -5,11 +5,11 @@ public static class PhoneNumber
 	public static (bool IsNewYork, bool IsFake, string LocalNumber) Analyze(string phoneNumber)
 	{
 		string[] numberParts = phoneNumber.Split('-');
-        return (numberParts[0] == "212", numberParts[1] == "555", numberParts[2]);
+		return (numberParts[0] == "212", numberParts[1] == "555", numberParts[2]);
 	}
 
 	public static bool IsFake((bool IsNewYork, bool IsFake, string LocalNumber) phoneNumberInfo)
 	{
-		throw new NotImplementedException($"Please implement the (static) PhoneNumber.IsFake() method");
+		return phoneNumberInfo.IsFake;
 	}
 }
