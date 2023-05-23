4b9b7d8d0f342ca686cf782e194c47bc261aa6e9
OnkelPony <zkurvenej@outlook.com>
2023-05-23 22:21:08 +0200

    cs_log-analysis complete.

--- csharp/log-analysis/LogAnalysis.cs
+++ csharp/log-analysis/LogAnalysis.cs
@@ -2,15 +2,27 @@ using System;
 
 public static class LogAnalysis
 {
-    // TODO: define the 'SubstringAfter()' extension method on the `string` type
-public static string SubstringAfter(this string str, string separator)
-{
-    int i = str.IndexOf(separator);
-    return i != -1? str.Substring(i + separator.Length) : str;
-}
-    // TODO: define the 'SubstringBetween()' extension method on the `string` type
-
-    // TODO: define the 'Message()' extension method on the `string` type
-
-    // TODO: define the 'LogLevel()' extension method on the `string` type
+	// TODO: define the 'SubstringAfter()' extension method on the `string` type
+	public static string SubstringAfter(this string str, string separator)
+	{
+		int i = str.IndexOf(separator);
+		return i != -1 ? str.Substring(i + separator.Length) : str;
+	}
+	// TODO: define the 'SubstringBetween()' extension method on the `string` type
+	public static string SubstringBetween(this string str, string before, string after)
+	{
+		int i = str.IndexOf(before);
+		int j = str.IndexOf(after);
+		return i != -1 && j != -1 ? str.Substring(i + before.Length, j - i - before.Length) : str;
+	}
+	// TODO: define the 'Message()' extension method on the `string` type
+    public static string Message(this string str)
+    {
+        return str.Split(':')[1].Trim();
+    }
+	// TODO: define the 'LogLevel()' extension method on the `string` type
+    public static string LogLevel(this string str)
+    {
+        return str.SubstringBetween("[", "]");
+    }
 }
