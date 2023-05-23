98ce10d7b445bf2474e84ae7944b6ab7cc3ad207
OnkelPony <zkurvenej@outlook.com>
2023-05-23 19:56:34 +0200

    cs_log-analysis SubstringAfter() complete.

--- csharp/log-analysis/LogAnalysis.cs
+++ csharp/log-analysis/LogAnalysis.cs
@@ -1,11 +1,15 @@
 using System;
 
-public static class LogAnalysis 
+public static class LogAnalysis
 {
     // TODO: define the 'SubstringAfter()' extension method on the `string` type
-
+public static string SubstringAfter(this string str, string separator)
+{
+    int i = str.IndexOf(separator);
+    return i != -1? str.Substring(i + separator.Length) : str;
+}
     // TODO: define the 'SubstringBetween()' extension method on the `string` type
-    
+
     // TODO: define the 'Message()' extension method on the `string` type
 
     // TODO: define the 'LogLevel()' extension method on the `string` type
