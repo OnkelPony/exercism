55168c371951c7cd4be7dc7b3053f38f336672a5
OnkelPony <zkurvenej@outlook.com>
2023-05-23 19:19:43 +0200

    cs_annalyns-infiltration complete.

--- csharp/annalyns-infiltration/AnnalynsInfiltration.cs
+++ csharp/annalyns-infiltration/AnnalynsInfiltration.cs
@@ -2,23 +2,23 @@ using System;
 
 static class QuestLogic
 {
-    public static bool CanFastAttack(bool knightIsAwake)
-    {
-        throw new NotImplementedException("Please implement the (static) QuestLogic.CanFastAttack() method");
-    }
+	public static bool CanFastAttack(bool knightIsAwake)
+	{
+		return !knightIsAwake;
+	}
 
-    public static bool CanSpy(bool knightIsAwake, bool archerIsAwake, bool prisonerIsAwake)
-    {
-        throw new NotImplementedException("Please implement the (static) QuestLogic.CanSpy() method");
-    }
+	public static bool CanSpy(bool knightIsAwake, bool archerIsAwake, bool prisonerIsAwake)
+	{
+		return knightIsAwake || archerIsAwake || prisonerIsAwake;
+	}
 
-    public static bool CanSignalPrisoner(bool archerIsAwake, bool prisonerIsAwake)
-    {
-        throw new NotImplementedException("Please implement the (static) QuestLogic.CanSignalPrisoner() method");
-    }
+	public static bool CanSignalPrisoner(bool archerIsAwake, bool prisonerIsAwake)
+	{
+		return !archerIsAwake && prisonerIsAwake;
+	}
 
-    public static bool CanFreePrisoner(bool knightIsAwake, bool archerIsAwake, bool prisonerIsAwake, bool petDogIsPresent)
-    {
-        throw new NotImplementedException("Please implement the (static) QuestLogic.CanFreePrisoner() method");
-    }
+	public static bool CanFreePrisoner(bool knightIsAwake, bool archerIsAwake, bool prisonerIsAwake, bool petDogIsPresent)
+	{
+		return (petDogIsPresent && !archerIsAwake) || (!knightIsAwake && !archerIsAwake && prisonerIsAwake);
+	}
 }
