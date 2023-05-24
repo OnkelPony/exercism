2b5606b3315332af39f96a2807dd6ae65b8f924e
OnkelPony <zkurvenej@outlook.com>
2023-05-24 17:54:37 +0200

    cs_tim-from-marketing complete.

--- csharp/tim-from-marketing/TimFromMarketing.cs
+++ csharp/tim-from-marketing/TimFromMarketing.cs
@@ -2,8 +2,16 @@ using System;
 
 static class Badge
 {
-    public static string Print(int? id, string name, string? department)
-    {
-        throw new NotImplementedException("Please implement the (static) Badge.Print() method");
-    }
+	public static string Print(int? id, string name, string? department)
+	{
+		if (department == null)
+		{
+			department = "OWNER";
+		}
+		if (id == null)
+		{
+					return $"{name} - {department.ToUpper()}";
+		}
+		return $"[{id}] - {name} - {department.ToUpper()}";
+	}
 }
