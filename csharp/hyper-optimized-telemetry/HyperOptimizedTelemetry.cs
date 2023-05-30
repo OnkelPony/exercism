a07813003162898f36601a9e307c35717da3c262
OnkelPony <zkurvenej@outlook.com>
2023-05-30 22:47:42 +0200

    cs_hyper-optimized-telemetry ToBuffer() started.

--- csharp/hyper-optimized-telemetry/HyperOptimizedTelemetry.cs
+++ csharp/hyper-optimized-telemetry/HyperOptimizedTelemetry.cs
@@ -2,13 +2,20 @@ using System;
 
 public static class TelemetryBuffer
 {
-    public static byte[] ToBuffer(long reading)
-    {
-        throw new NotImplementedException("Please implement the static TelemetryBuffer.ToBuffer() method");
-    }
+	public static byte[] ToBuffer(long reading)
+	{
+        byte[] result = new byte[9];
+        if (reading <= int.MinValue || reading > uint.MaxValue)
+        {
+        result[0] = 256 - 8;
+        }
+        else if (reading <=
+		 BitConverter.GetBytes(reading).CopyTo(result, 1);
+         return result;
+	}
 
-    public static long FromBuffer(byte[] buffer)
-    {
-        throw new NotImplementedException("Please implement the static TelemetryBuffer.FromBuffer() method");
-    }
+	public static long FromBuffer(byte[] buffer)
+	{
+		throw new NotImplementedException("Please implement the static TelemetryBuffer.FromBuffer() method");
+	}
 }
