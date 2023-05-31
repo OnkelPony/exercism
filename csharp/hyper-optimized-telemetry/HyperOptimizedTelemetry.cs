874243871ab4f4962373bff770389829ce2db355
OnkelPony <zkurvenej@outlook.com>
2023-05-31 20:42:24 +0200

    cs_hyper-optimized-telemetry ToBuffer() complete.

--- csharp/hyper-optimized-telemetry/HyperOptimizedTelemetry.cs
+++ csharp/hyper-optimized-telemetry/HyperOptimizedTelemetry.cs
@@ -4,14 +4,33 @@ public static class TelemetryBuffer
 {
 	public static byte[] ToBuffer(long reading)
 	{
-        byte[] result = new byte[9];
-        if (reading <= int.MinValue || reading > uint.MaxValue)
-        {
-        result[0] = 256 - 8;
-        }
-        else if (reading <=
-		 BitConverter.GetBytes(reading).CopyTo(result, 1);
-         return result;
+		byte[] result = new byte[9];
+		if (reading < int.MinValue || reading > uint.MaxValue)
+		{
+			result[0] = 256 - 8;
+			BitConverter.GetBytes(reading).CopyTo(result, 1);
+		}
+		else if (reading > int.MaxValue)
+		{
+			result[0] = 4;
+			Array.Copy(BitConverter.GetBytes(reading), 0, result, 1, 4);
+		}
+		else if (reading < short.MinValue || reading > ushort.MaxValue)
+		{
+			result[0] = 256 - 4;
+			Array.Copy(BitConverter.GetBytes(reading), 0, result, 1, 4);
+		}
+		else if (reading >= 0)
+		{
+			result[0] = 2;
+			Array.Copy(BitConverter.GetBytes(reading), 0, result, 1, 2);
+		}
+		else
+		{
+			result[0] = 256 - 2;
+			Array.Copy(BitConverter.GetBytes(reading), 0, result, 1, 2);
+		}
+		return result;
 	}
 
 	public static long FromBuffer(byte[] buffer)
