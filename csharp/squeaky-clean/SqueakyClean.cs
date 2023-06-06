077ae8b326fa511c7e36ad15345a42315c49302d
OnkelPony <zkurvenej@outlook.com>
2023-06-06 22:51:47 +0200

    cs_squeaky-clean complete.

--- csharp/squeaky-clean/SqueakyClean.cs
+++ csharp/squeaky-clean/SqueakyClean.cs
@@ -2,8 +2,40 @@ using System;
 
 public static class Identifier
 {
-    public static string Clean(string identifier)
-    {
-        throw new NotImplementedException($"Please implement the (static) Identifier.Clean() method");
-    }
+	public static string Clean(string identifier)
+	{
+		string result = "";
+		bool toCamel = false;
+		foreach (char ch in identifier)
+		{
+			if (ch == ' ')
+			{
+				result += '_';
+			}
+			else if (ch == '-')
+			{
+				toCamel = true;
+			}
+			else if (ch == '\0')
+			{
+				result += "CTRL";
+			}
+			else if (!Char.IsLetter(ch) || (ch >= '\u03B1' && ch <= '\u03C9'))
+			{
+			}
+			else
+			{
+				if (toCamel)
+				{
+					result += Char.ToUpperInvariant(ch);
+					toCamel = false;
+				}
+				else
+				{
+					result += ch;
+				}
+			}
+		}
+		return result;
+	}
 }
