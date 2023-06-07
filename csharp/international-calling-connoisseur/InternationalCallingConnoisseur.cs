fa2e34b291f1123dea869a17a45ccacfa63ef26d
OnkelPony <zkurvenej@outlook.com>
2023-06-07 23:10:28 +0200

    cs_international-calling-connoisseur CheckCodeExists() complete.

--- csharp/international-calling-connoisseur/InternationalCallingConnoisseur.cs
+++ csharp/international-calling-connoisseur/InternationalCallingConnoisseur.cs
@@ -3,52 +3,69 @@ using System.Collections.Generic;
 
 public static class DialingCodes
 {
-    public static Dictionary<int, string> GetEmptyDictionary()
-    {
-        throw new NotImplementedException($"Please implement the (static) GetEmptyDictionary() method");
-    }
+	private static Dictionary<int, string> emptyCodes = new Dictionary<int, string>();
+	private static Dictionary<int, string> smallCodes = new Dictionary<int, string>{
+		{1, "United States of America"},
+		{55, "Brasil"},
+		{91, "India"}
+	};
 
-    public static Dictionary<int, string> GetExistingDictionary()
-    {
-        throw new NotImplementedException($"Please implement the (static) GetExistingDictionary() method");
-    }
+	public static Dictionary<int, string> GetEmptyDictionary()
+	{
+		return emptyCodes;
+	}
 
-    public static Dictionary<int, string> AddCountryToEmptyDictionary(int countryCode, string countryName)
-    {
-        throw new NotImplementedException($"Please implement the (static) AddCountryToEmptyDictionary() method");
-    }
+	public static Dictionary<int, string> GetExistingDictionary()
+	{
+		return smallCodes;
+	}
 
-    public static Dictionary<int, string> AddCountryToExistingDictionary(
-        Dictionary<int, string> existingDictionary, int countryCode, string countryName)
-    {
-        throw new NotImplementedException($"Please implement the (static) AddCountryToExistingDictionary() method");
-    }
+	public static Dictionary<int, string> AddCountryToEmptyDictionary(int countryCode, string countryName)
+	{
+		var codes = new Dictionary<int, string>();
+		codes.Add(countryCode, countryName);
+		return codes;
+	}
 
-    public static string GetCountryNameFromDictionary(
-        Dictionary<int, string> existingDictionary, int countryCode)
-    {
-        throw new NotImplementedException($"Please implement the (static) GetCountryNameFromDictionary() method");
-    }
+	public static Dictionary<int, string> AddCountryToExistingDictionary(
+		Dictionary<int, string> existingDictionary, int countryCode, string countryName)
+	{
+		if (!existingDictionary.ContainsKey(countryCode))
+		{
+			existingDictionary.Add(countryCode, countryName);
+		}
+		return existingDictionary;
+	}
 
-    public static bool CheckCodeExists(Dictionary<int, string> existingDictionary, int countryCode)
-    {
-        throw new NotImplementedException($"Please implement the (static) CheckCodeExists() method");
-    }
+	public static string GetCountryNameFromDictionary(
+		Dictionary<int, string> existingDictionary, int countryCode)
+	{
+		if (!existingDictionary.ContainsKey(countryCode))
+		{
+			return string.Empty;
+		}
+		return existingDictionary[countryCode];
+	}
 
-    public static Dictionary<int, string> UpdateDictionary(
-        Dictionary<int, string> existingDictionary, int countryCode, string countryName)
-    {
-        throw new NotImplementedException($"Please implement the (static) UpdateDictionary() method");
-    }
+	public static bool CheckCodeExists(Dictionary<int, string> existingDictionary, int countryCode)
+	{
+		return existingDictionary.ContainsKey(countryCode);
+	}
 
-    public static Dictionary<int, string> RemoveCountryFromDictionary(
-        Dictionary<int, string> existingDictionary, int countryCode)
-    {
-        throw new NotImplementedException($"Please implement the (static) RemoveCountryFromDictionary() method");
-    }
+	public static Dictionary<int, string> UpdateDictionary(
+		Dictionary<int, string> existingDictionary, int countryCode, string countryName)
+	{
+		throw new NotImplementedException($"Please implement the (static) UpdateDictionary() method");
+	}
 
-    public static string FindLongestCountryName(Dictionary<int, string> existingDictionary)
-    {
-        throw new NotImplementedException($"Please implement the (static) FindLongestCountryName() method");
-    }
+	public static Dictionary<int, string> RemoveCountryFromDictionary(
+		Dictionary<int, string> existingDictionary, int countryCode)
+	{
+		throw new NotImplementedException($"Please implement the (static) RemoveCountryFromDictionary() method");
+	}
+
+	public static string FindLongestCountryName(Dictionary<int, string> existingDictionary)
+	{
+		throw new NotImplementedException($"Please implement the (static) FindLongestCountryName() method");
+	}
 }
