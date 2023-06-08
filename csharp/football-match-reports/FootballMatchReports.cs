b6abf00cf5227f323608efaa6b28ed0e3429289a
OnkelPony <zkurvenej@outlook.com>
2023-06-08 22:33:38 +0200

    cs_football-match-reports complete.

--- csharp/football-match-reports/FootballMatchReports.cs
+++ csharp/football-match-reports/FootballMatchReports.cs
@@ -2,13 +2,38 @@ using System;
 
 public static class PlayAnalyzer
 {
-    public static string AnalyzeOnField(int shirtNum)
-    {
-        throw new NotImplementedException($"Please implement the (static) PlayAnalyzer.AnalyzeOnField() method");
-    }
+	public static string AnalyzeOnField(int shirtNum)
+	{
+		switch (shirtNum)
+		{
+			case 1: return "goalie";
+			case 2: return "left back";
+			case 3:
+			case 4: return "center back";
+			case 5: return "right back";
+			case 6:
+			case 7:
+			case 8: return "midfielder";
+			case 9: return "left wing";
+			case 10: return "striker";
+			case 11: return "right wing";
+			default: throw new ArgumentOutOfRangeException();
+		}
+	}
 
-    public static string AnalyzeOffField(object report)
-    {
-        throw new NotImplementedException($"Please implement the (static) PlayAnalyzer.AnalyzeOffField() method");
-    }
+	public static string AnalyzeOffField(object report)
+	{
+		switch (report)
+		{
+			case int supporters: return $"There are {supporters} supporters at the match.";
+			case string announcement: return announcement;
+			case Foul foul: return foul.GetDescription();
+			case Injury injury: return $"Oh no! {injury.GetDescription()} Medics are on the field.";
+            case Incident incident: return incident.GetDescription();
+			case Manager manager:
+				string club = manager.Club != null ? $" ({manager.Club})" : "";
+				return $"{manager.Name}{club}";
+			default: throw new ArgumentException();
+		}
+	}
 }
