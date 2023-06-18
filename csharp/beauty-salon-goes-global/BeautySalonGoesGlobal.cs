c664ead88ac1cddf1e1d937f696e54390a7e31e7
Jiří Martínek <zkurvenej@outlook.com>
2023-06-18 15:10:02 +0200

    cs_beauty-salon-goes-global GetAlertTime() complete.

--- csharp/beauty-salon-goes-global/BeautySalonGoesGlobal.cs
+++ csharp/beauty-salon-goes-global/BeautySalonGoesGlobal.cs
@@ -55,12 +55,45 @@ public static class Appointment
     }
     public static DateTime GetAlertTime(DateTime appointment, AlertLevel alertLevel)
     {
-        throw new NotImplementedException("Please implement the (static) Appointment.GetAlertTime() method");
+        switch (alertLevel)
+        {
+            case AlertLevel.Early: return appointment.AddDays(-1);
+            case AlertLevel.Standard: return appointment.AddHours(-1).AddMinutes(-45);
+            case AlertLevel.Late: return appointment.AddMinutes(-30);
+            default: return appointment;
+        }
     }
 
     public static bool HasDaylightSavingChanged(DateTime dt, Location location)
     {
-        throw new NotImplementedException("Please implement the (static) Appointment.HasDaylightSavingChanged() method");
+        if (OperatingSystem.IsWindows())
+        {
+            switch (location)
+            {
+                case Location.London:
+                    return TimeZoneInfo.ConvertTimeToUtc(DateTime.Parse(appointmentDateDescription), TimeZoneInfo.FindSystemTimeZoneById("GMT Standard Time"));
+                case Location.NewYork:
+                    return TimeZoneInfo.ConvertTimeToUtc(DateTime.Parse(appointmentDateDescription), TimeZoneInfo.FindSystemTimeZoneById("Eastern Standard Time"));
+                case Location.Paris:
+                    return TimeZoneInfo.ConvertTimeToUtc(DateTime.Parse(appointmentDateDescription), TimeZoneInfo.FindSystemTimeZoneById("W. Europe Standard Time"));
+                default:
+                    return DateTime.Parse(appointmentDateDescription);
+            }
+        }
+        else
+        {
+            switch (location)
+            {
+                case Location.London:
+                    return TimeZoneInfo.ConvertTimeToUtc(DateTime.Parse(appointmentDateDescription), TimeZoneInfo.FindSystemTimeZoneById("Europe/London"));
+                case Location.NewYork:
+                    return TimeZoneInfo.ConvertTimeToUtc(DateTime.Parse(appointmentDateDescription), TimeZoneInfo.FindSystemTimeZoneById("America/New_York"));
+                case Location.Paris:
+                    return TimeZoneInfo.ConvertTimeToUtc(DateTime.Parse(appointmentDateDescription), TimeZoneInfo.FindSystemTimeZoneById("Europe/Paris"));
+                default:
+                    return DateTime.Parse(appointmentDateDescription);
+            }
+        }
     }
 
     public static DateTime NormalizeDateTime(string dtStr, Location location)
