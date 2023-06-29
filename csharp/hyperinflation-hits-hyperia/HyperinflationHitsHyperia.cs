beb7918a47d2a809f131417b8dd76718cfdd4cc7
OnkelPony <zkurvenej@outlook.com>
2023-06-29 19:57:57 +0200

    cs_hyperinflation-hits-hyperia DisplayDenomination() complete.

--- csharp/hyperinflation-hits-hyperia/HyperinflationHitsHyperia.cs
+++ csharp/hyperinflation-hits-hyperia/HyperinflationHitsHyperia.cs
@@ -2,18 +2,31 @@ using System;
 
 public static class CentralBank
 {
-    public static string DisplayDenomination(long @base, long multiplier)
-    {
-        throw new NotImplementedException($"Please implement the (static) CentralBank.DisplayDenomination() method");
-    }
+	public static string DisplayDenomination(long @base, long multiplier)
+	{
+		long result;
+		try
+		{
+			result = checked(@base * multiplier);
+		}
+		catch (OverflowException)
+		{
+			return "*** Too Big ***";
+		}
+		return $"{result}";
+	}
 
-    public static string DisplayGDP(float @base, float multiplier)
-    {
-        throw new NotImplementedException($"Please implement the (static) CentralBank.DisplayGDP() method");
-    }
+	public static string DisplayGDP(float @base, float multiplier)
+	{
+		float result = @base * multiplier;
+		if (Double.IsInfinity(result)){
+			return "*** Too Big ***";
+		}
+		return $"{result}";
+	}
 
-    public static string DisplayChiefEconomistSalary(decimal salaryBase, decimal multiplier)
-    {
-        throw new NotImplementedException($"Please implement the (static) CentralBank.DisplayChiefEconomistSalary() method");
-    }
+	public static string DisplayChiefEconomistSalary(decimal salaryBase, decimal multiplier)
+	{
+		throw new NotImplementedException($"Please implement the (static) CentralBank.DisplayChiefEconomistSalary() method");
+	}
 }
