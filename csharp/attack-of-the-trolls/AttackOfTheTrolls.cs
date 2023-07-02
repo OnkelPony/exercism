4e562a41b82bc661b6776f4ac15eb2b93ea1ccbc
Jiří Martínek <zkurvenej@outlook.com>
2023-07-02 14:55:32 +0200

    cs_attack-of-the-trolls Grant() complete.

--- csharp/attack-of-the-trolls/AttackOfTheTrolls.cs
+++ csharp/attack-of-the-trolls/AttackOfTheTrolls.cs
@@ -1,24 +1,43 @@
 using System;
 
-// TODO: define the 'AccountType' enum
+enum AccountType : short
+{
+    Guest = 1,
+    User = 3,
+    Moderator = 7
+}
 
-// TODO: define the 'Permission' enum
-
+[Flags]
+enum Permission : short
+{
+    None = 0,
+    Read = 1,
+    Write = 2,
+    Delete = 4,
+    All = 7,
+}
 static class Permissions
 {
     public static Permission Default(AccountType accountType)
     {
-        throw new NotImplementedException("Please implement the (static) Permissions.Default() method");
+        if (Enum.IsDefined(typeof(AccountType), accountType))
+        {
+            return (Permission)accountType;
+        }
+        else
+        {
+            return Permission.None;
+        }
     }
 
     public static Permission Grant(Permission current, Permission grant)
     {
-        throw new NotImplementedException("Please implement the (static) Permissions.Grant() method");
+        return (Permission)((short)current | (short)grant);
     }
 
     public static Permission Revoke(Permission current, Permission revoke)
     {
-        throw new NotImplementedException("Please implement the (static) Permissions.Revoke() method");
+        return (Permission)((short)current - revoke);
     }
 
     public static bool Check(Permission current, Permission check)
