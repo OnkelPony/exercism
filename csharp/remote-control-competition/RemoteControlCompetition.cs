783a918b8c4d0d846f1e5c599f953e2e84c40adb
Jiří Martínek <jmartinek@corpus.cz>
2023-07-04 15:55:15 +0200

    cs_remote-control-competition half complete.

--- csharp/remote-control-competition/RemoteControlCompetition.cs
+++ csharp/remote-control-competition/RemoteControlCompetition.cs
@@ -4,40 +4,46 @@ using System.Collections.Generic;
 // TODO implement the IRemoteControlCar interface
 public interface IRemoteControlCar
 {
-    void 
+    public int DistanceTravelled { get; }
+	void Drive();
 }
 
-public class ProductionRemoteControlCar
+public class ProductionRemoteControlCar : IRemoteControlCar, IComparable<T>
 {
-    public int DistanceTravelled { get; private set; }
-    public int NumberOfVictories { get; set; }
+	public int DistanceTravelled { get; private set; }
+	public int NumberOfVictories { get; set; }
 
-    public void Drive()
-    {
-        DistanceTravelled += 10;
-    }
+	public void Drive()
+	{
+		DistanceTravelled += 10;
+	}
+
+	int IComparable<T>.CompareTo(T? other)
+	{
+		throw new NotImplementedException();
+	}
 }
 
-public class ExperimentalRemoteControlCar
+public class ExperimentalRemoteControlCar : IRemoteControlCar
 {
-    public int DistanceTravelled { get; private set; }
+	public int DistanceTravelled { get; private set; }
 
-    public void Drive()
-    {
-        DistanceTravelled += 20;
-    }
+	public void Drive()
+	{
+		DistanceTravelled += 20;
+	}
 }
 
 public static class TestTrack
 {
-    public static void Race(IRemoteControlCar car)
-    {
-        throw new NotImplementedException($"Please implement the (static) TestTrack.Race() method");
-    }
+	public static void Race(IRemoteControlCar car)
+	{
+        car.Drive();
+	}
 
-    public static List<ProductionRemoteControlCar> GetRankedCars(ProductionRemoteControlCar prc1,
-        ProductionRemoteControlCar prc2)
-    {
-        throw new NotImplementedException($"Please implement the (static) TestTrack.GetRankedCars() method");
-    }
+	public static List<ProductionRemoteControlCar> GetRankedCars(ProductionRemoteControlCar prc1,
+		ProductionRemoteControlCar prc2)
+	{
+		throw new NotImplementedException($"Please implement the (static) TestTrack.GetRankedCars() method");
+	}
 }
