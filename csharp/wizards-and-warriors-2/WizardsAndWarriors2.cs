698cf5f692b19d2f7b4af222592aab68ccf595f8
Jiří Martínek <zkurvenej@outlook.com>
2023-07-16 12:44:05 +0200

    cs_wizards-and-warriors-2 complete.

--- csharp/wizards-and-warriors-2/WizardsAndWarriors2.cs
+++ csharp/wizards-and-warriors-2/WizardsAndWarriors2.cs
@@ -2,18 +2,18 @@ using System;
 
 static class GameMaster
 {
-	public static string Describe(Character character)
-	{
-		return $"You're a level {character.Level} {character.Class} with {character.HitPoints} hit points.";
-	}
+    public static string Describe(Character character)
+    {
+        return $"You're a level {character.Level} {character.Class} with {character.HitPoints} hit points.";
+    }
 
-	public static string Describe(Destination destination)
-	{
+    public static string Describe(Destination destination)
+    {
         return $"You've arrived at {destination.Name}, which has {destination.Inhabitants} inhabitants.";
-	}
+    }
 
-	public static string Describe(TravelMethod travelMethod)
-	{
+    public static string Describe(TravelMethod travelMethod)
+    {
         string method = travelMethod switch
         {
             TravelMethod.Walking => "by walking",
@@ -21,34 +21,34 @@ static class GameMaster
             _ => "whatever"
         };
         return $"You're traveling to your destination {method}.";
-	}
+    }
 
-	public static string Describe(Character character, Destination destination, TravelMethod travelMethod)
-	{
-		throw new NotImplementedException("Please implement the (static) GameMaster.Describe(Character, Destination, TravelMethod) method");
-	}
+    public static string Describe(Character character, Destination destination, TravelMethod travelMethod)
+    {
+        return $"{Describe(character)} {Describe(travelMethod)} {Describe(destination)}";
+    }
 
-	public static string Describe(Character character, Destination destination)
-	{
-		throw new NotImplementedException("Please implement the (static) GameMaster.Describe(Character, Destination) method");
-	}
+    public static string Describe(Character character, Destination destination)
+    {
+        return Describe(character, destination, TravelMethod.Walking);
+    }
 }
 
 class Character
 {
-	public string Class { get; set; }
-	public int Level { get; set; }
-	public int HitPoints { get; set; }
+    public string Class { get; set; }
+    public int Level { get; set; }
+    public int HitPoints { get; set; }
 }
 
 class Destination
 {
-	public string Name { get; set; }
-	public int Inhabitants { get; set; }
+    public string Name { get; set; }
+    public int Inhabitants { get; set; }
 }
 
 enum TravelMethod
 {
-	Walking,
-	Horseback
+    Walking,
+    Horseback
 }
