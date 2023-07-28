c54145f93bc66fa34cbf83d6421b77f07cd60687
Jiří Martínek <zkurvenej@outlook.com>
2023-07-28 20:31:54 +0200

    cs_high-school-sweethearts DisplaySingleLine() complete.

--- csharp/high-school-sweethearts/HighSchoolSweethearts.cs
+++ csharp/high-school-sweethearts/HighSchoolSweethearts.cs
@@ -4,7 +4,11 @@ public static class HighSchoolSweethearts
 {
     public static string DisplaySingleLine(string studentA, string studentB)
     {
-        throw new NotImplementedException($"Please implement the (static) HighSchoolSweethearts.DisplaySingleLine() method");
+        const int width = 61;
+        string result = $"{studentA} ♡ {studentB}";
+        int leftPadding = (width - result.Length) / 2 - 1;
+        int rightPadding = (width - leftPadding - result.Length);
+        return $"{new string(' ', leftPadding)}{result}{new string(' ', rightPadding)}";
     }
 
     public static string DisplayBanner(string studentA, string studentB)
