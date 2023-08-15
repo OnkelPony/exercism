89a84d8f7efffbe137d88af322ade223576f6171
Jiří Martínek <jmartinek@corpus.cz>
2023-08-15 18:23:51 +0200

    cs_parsing-log-files IsValidLine() partially complete.

--- csharp/parsing-log-files/ParsingLogFiles.cs
+++ csharp/parsing-log-files/ParsingLogFiles.cs
@@ -5,7 +5,7 @@ public class LogParser
 {
     public bool IsValidLine(string text)
     {
-        throw new NotImplementedException($"Please implement the LogParser.IsValidLine() method");
+        return Regex.IsMatch(text, @"^\[INF\].*");
     }
 
     public string[] SplitLogLine(string text)
