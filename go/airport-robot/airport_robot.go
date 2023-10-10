ec5290ee02926945927b7f600f8308bc2509ac45
OnkelPony <zkurvenej@outlook.com>
2023-10-10 21:27:20 +0200

    go_airport-robot  Greeter interface started.

--- go/airport-robot/airport_robot.go
+++ go/airport-robot/airport_robot.go
@@ -3,3 +3,10 @@ package airportrobot
 // Write your code here.
 // This exercise does not have tests for each individual task.
 // Try to solve all the tasks first before running the tests.
+
+type Greeter interface {
+	Greet(name string) string
+	LanguageName() string
+}
+
+func SayHello(name string, greeter Greeter) string {
