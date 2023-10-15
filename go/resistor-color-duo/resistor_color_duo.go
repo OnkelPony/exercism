b38c9ea41a5bab4f84d0c9915eafd7a86cf8d569
Jiří Martínek <zkurvenej@outlook.com>
2023-10-15 22:10:06 +0200

    go_resistor-color-duo done.

---
+++ go/resistor-color-duo/resistor_color_duo.go
@@ -0,0 +1,19 @@
+package resistorcolorduo
+
+var codes = map[string]int{
+	"black":  0,
+	"brown":  1,
+	"red":    2,
+	"orange": 3,
+	"yellow": 4,
+	"green":  5,
+	"blue":   6,
+	"violet": 7,
+	"grey":   8,
+	"white":  9,
+}
+
+// Value should return the resistance value of a resistor with a given colors.
+func Value(colors []string) int {
+	return 10*codes[colors[0]] + codes[colors[1]]
+}
