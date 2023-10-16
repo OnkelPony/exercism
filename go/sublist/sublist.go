73e5da1d59f153445b0d2caa1c6ab9524f86cd7a
Jiří Martínek <jmartinek@corpus.cz>
2023-10-16 18:11:00 +0200

    go_sublist tests failing.

--- go/sublist/sublist.go
+++ go/sublist/sublist.go
@@ -3,5 +3,25 @@ package sublist
 // Relation type is defined in relations.go file.
 
 func Sublist(l1, l2 []int) Relation {
-	
+	if isEqual(l1, l2) {
+		return RelationEqual
+	} else if isSublist(l1, l2) {
+		return RelationSublist
+	} else if isSuperlist(l1, l2) {
+		return RelationSuperlist
+	} else {
+		return RelationUnequal
+	}
+}
+
+func isEqual(l1, l2 []int) bool {
+	return false
+}
+
+func isSublist(l1, l2 []int) bool {
+	return false
+}
+
+func isSuperlist(l1, l2 []int) bool {
+	return false
 }
