046bc86fb91dff82eaa4b31500fb77c07f3e6e96
Jiří Martínek <jmartinek@corpus.cz>
2023-10-16 19:35:10 +0200

    go_sublist done.

--- go/sublist/sublist.go
+++ go/sublist/sublist.go
@@ -15,13 +15,43 @@ func Sublist(l1, l2 []int) Relation {
 }
 
 func isEqual(l1, l2 []int) bool {
-	return false
+	if len(l1) != len(l2) {
+		return false
+	}
+	for i := range l1 {
+		if l1[i] != l2[i] {
+			return false
+		}
+	}
+	return true
 }
 
 func isSublist(l1, l2 []int) bool {
+	equals := false
+	short := len(l1)
+	long := len(l2)
+	if short > long {
+		return false
+	}	
+	if short == 0 {
+		return true
+	}
+	for o := 0; o <= long-short; o++ {
+		for i := 0; i < short; i++ {
+			if l1[i] == l2[o+i] {
+				equals = true
+			} else {
+				equals = false
+				break
+			}
+		}
+		if equals {
+			return equals
+		}
+	}
 	return false
 }
 
 func isSuperlist(l1, l2 []int) bool {
-	return false
+	return isSublist(l2, l1)
 }
