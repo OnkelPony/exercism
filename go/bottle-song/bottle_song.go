3e74eaeceba1c08c77f7e6ee96463bdd5b514317
Jiří Martínek <jmartinek@corpus.cz>
2023-10-17 19:03:53 +0200

    go_bottle-song fixing plural in one bottle remains.

--- go/bottle-song/bottle_song.go
+++ go/bottle-song/bottle_song.go
@@ -1,5 +1,29 @@
 package bottlesong
 
+import (
+	"fmt"
+	"strings"
+)
+
+const (
+	firstAndSecondRow = "green bottles hanging on the wall,"
+	thirdRow = "And if one green bottle should accidentally fall,"
+	fourthRowStart = "There'll be"
+	fourthRowEndOne = "green bottle hanging on the wall."
+	fourthRowEnd = "green bottles hanging on the wall."
+)
+
 func Recite(startBottles, takeDown int) []string {
-	panic("Please implement the Recite function")
+	numbers := []string{"no", "One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten"}
+	var result []string
+	for i := 0; i < takeDown; i++ {
+		result = append(result, fmt.Sprintf("%s %s", numbers[startBottles - i], firstAndSecondRow))
+		result = append(result, fmt.Sprintf("%s %s", numbers[startBottles - i], firstAndSecondRow))
+		result = append(result, thirdRow)
+		result = append(result, fmt.Sprintf("%s %s %s", fourthRowStart, strings.ToLower(numbers[startBottles - i - 1]), fourthRowEnd))
+		if i + 1 < takeDown {
+			result = append(result, "")
+		}
+	}
+	return result
 }
