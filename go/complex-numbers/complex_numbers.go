7f5cb08370c8456433622edeaa80e05229142404
Jiří Martínek <jmartinek@corpus.cz>
2023-10-18 19:20:44 +0200

    go_complex-numbers Divide() done.

--- go/complex-numbers/complex_numbers.go
+++ go/complex-numbers/complex_numbers.go
@@ -1,33 +1,39 @@
 package complexnumbers
 
 // Define the Number type here.
+type Number struct {
+	real float64
+	imag float64
+}
 
 func (n Number) Real() float64 {
-	panic("Please implement the Real method")
+	return n.real
 }
 
 func (n Number) Imaginary() float64 {
-	panic("Please implement the Imaginary method")
+	return n.imag
 }
 
 func (n1 Number) Add(n2 Number) Number {
-	panic("Please implement the Add method")
+	return Number{n1.real + n2.real, n1.imag + n2.imag}
 }
 
 func (n1 Number) Subtract(n2 Number) Number {
-	panic("Please implement the Subtract method")
+	return Number{n1.real - n2.real, n1.imag - n2.imag}
 }
 
 func (n1 Number) Multiply(n2 Number) Number {
-	panic("Please implement the Multiply method")
+	return Number{n1.real*n2.real - n1.imag*n2.imag, n1.imag*n2.real + n1.real*n2.imag}
 }
 
 func (n Number) Times(factor float64) Number {
-	panic("Please implement the Times method")
+	return n.Multiply(Number{factor, 0})
 }
 
 func (n1 Number) Divide(n2 Number) Number {
-	panic("Please implement the Divide method")
+	real := (n1.real*n2.real + n1.imag*n2.imag) / (n2.real*n2.real + n2.imag*n2.imag)
+	imag := (n1.imag*n2.real - n1.real*n2.imag) / (n2.real*n2.real + n2.imag*n2.imag)
+	return Number{real, imag}
 }
 
 func (n Number) Conjugate() Number {
