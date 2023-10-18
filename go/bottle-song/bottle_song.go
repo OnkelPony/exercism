7a4440a10d36dddd64ae30cead5cd5a50d209e2d
Jiří Martínek <jmartinek@corpus.cz>
2023-10-18 18:40:23 +0200

    go_bottle-song done.

--- go/bottle-song/bottle_song.go
+++ go/bottle-song/bottle_song.go
@@ -6,22 +6,33 @@ import (
 )
 
 const (
-	firstAndSecondRow = "green bottles hanging on the wall,"
-	thirdRow = "And if one green bottle should accidentally fall,"
-	fourthRowStart = "There'll be"
-	fourthRowEndOne = "green bottle hanging on the wall."
-	fourthRowEnd = "green bottles hanging on the wall."
+	firstAndSecondRowOne = "green bottle hanging on the wall,"
+	firstAndSecondRow    = "green bottles hanging on the wall,"
+	thirdRow             = "And if one green bottle should accidentally fall,"
+	fourthRowStart       = "There'll be"
+	fourthRowEndOne      = "green bottle hanging on the wall."
+	fourthRowEnd         = "green bottles hanging on the wall."
 )
 
 func Recite(startBottles, takeDown int) []string {
 	numbers := []string{"no", "One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten"}
 	var result []string
 	for i := 0; i < takeDown; i++ {
-		result = append(result, fmt.Sprintf("%s %s", numbers[startBottles - i], firstAndSecondRow))
-		result = append(result, fmt.Sprintf("%s %s", numbers[startBottles - i], firstAndSecondRow))
+		bottles := startBottles - i
+		if bottles == 1 {
+			result = append(result, fmt.Sprintf("%s %s", numbers[bottles], firstAndSecondRowOne))
+			result = append(result, fmt.Sprintf("%s %s", numbers[bottles], firstAndSecondRowOne))
+		} else {
+			result = append(result, fmt.Sprintf("%s %s", numbers[bottles], firstAndSecondRow))
+			result = append(result, fmt.Sprintf("%s %s", numbers[bottles], firstAndSecondRow))
+		}
 		result = append(result, thirdRow)
-		result = append(result, fmt.Sprintf("%s %s %s", fourthRowStart, strings.ToLower(numbers[startBottles - i - 1]), fourthRowEnd))
-		if i + 1 < takeDown {
+		if bottles == 2 {
+			result = append(result, fmt.Sprintf("%s %s %s", fourthRowStart, strings.ToLower(numbers[bottles-1]), fourthRowEndOne))
+		} else {
+			result = append(result, fmt.Sprintf("%s %s %s", fourthRowStart, strings.ToLower(numbers[bottles-1]), fourthRowEnd))
+		}
+		if i+1 < takeDown {
 			result = append(result, "")
 		}
 	}
