7d1f60a50c31919737dac1074c14df028d73401f
Jiří Martínek <jmartinek@corpus.cz>
2023-10-25 19:10:32 +0200

    go_flatten-array nil cases remaining.

--- go/flatten-array/flatten_array.go
+++ go/flatten-array/flatten_array.go
@@ -3,17 +3,14 @@ package flatten
 import "reflect"
 
 func Flatten(nested interface{}) []interface{} {
-	result := []interface{}{}
-	if reflect.DeepEqual(nested, []interface{}{}) {
-		return result
-	}
-	slice := reflect.ValueOf(nested)
-	if slice.Kind() == reflect.Slice {
-		for i := 0; i < slice.Len(); i++ {
-
+	var result []interface{}
+	value := reflect.ValueOf(nested)
+	if value.Kind() == reflect.Slice {
+		for i := 0; i < value.Len(); i++ {
+			result = append(result, Flatten(value.Index(i).Interface())...)
 		}
+	} else {
+		result = append(result, nested)
 	}
 	return result
 }
-
-func newFlatter()
