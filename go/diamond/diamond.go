84b0692734ba1a2dd553d785e23a1e449292b3f1
Jiří Martínek <jmartinek@corpus.cz>
2023-11-01 18:57:27 +0100

    go_diamond empty result ready.

--- go/diamond/diamond.go
+++ go/diamond/diamond.go
@@ -1,5 +1,23 @@
 package diamond
 
+import (
+	"bytes"
+	"fmt"
+)
+
 func Gen(char byte) (string, error) {
-	panic("Please implement the Gen function")
+	if char < 'A' || char > 'Z' {
+		return "", fmt.Errorf("Invalid input character")
+	}
+	size := int(char-'A')*2 + 1
+	result := make([][]byte, size)
+	for i := range result {
+		result[i] = make([]byte, size)
+	}
+	for i := range result {
+		for j := range result[i] {
+			result[i][j] = ' '
+		}
+	}
+	return string(bytes.Join(result, []byte("\n"))), nil
 }
