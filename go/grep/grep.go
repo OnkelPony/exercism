a39b317c6a8f23fd27630f2f64b271faf00a6f5c
Jiří Martínek <zkurvenej@outlook.com>
2023-11-04 18:05:06 +0100

    go_grep one file, no flags complete.

--- go/grep/grep.go
+++ go/grep/grep.go
@@ -1,5 +1,30 @@
 package grep
 
+import (
+	"bufio"
+	"os"
+	"strings"
+)
+
 func Search(pattern string, flags, files []string) []string {
-	panic("Please implement the Search function")
+result := make([]string, 0)
+for _, f := range files {
+	found := findInfile(pattern, flags, f)
+	result = append(result, found...)
 }
+return result
+}
+
+func findInfile(pattern string, flags []string, f string) []string {
+	var result []string
+	file, _ := os.Open(f)
+	defer file.Close()
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		if strings.Contains(scanner.Text(), pattern) {
+			result = append(result, scanner.Text())
+		}
+	}
+	return result
+}
+
