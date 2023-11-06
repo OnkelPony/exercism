6361c3589f93850793f1f262da14e60055ceb859
Jiří Martínek <jmartinek@corpus.cz>
2023-11-06 19:43:59 +0100

    go_grep one file, -l flag done.

--- go/grep/grep.go
+++ go/grep/grep.go
@@ -2,29 +2,40 @@ package grep
 
 import (
 	"bufio"
+	"fmt"
 	"os"
+	"slices"
 	"strings"
 )
 
 func Search(pattern string, flags, files []string) []string {
-result := make([]string, 0)
-for _, f := range files {
-	found := findInfile(pattern, flags, f)
-	result = append(result, found...)
-}
-return result
+	result := make([]string, 0)
+	for _, f := range files {
+		found := findInfile(pattern, flags, f)
+		result = append(result, found...)
+	}
+	return result
 }
 
 func findInfile(pattern string, flags []string, f string) []string {
 	var result []string
+	var lineNum int
 	file, _ := os.Open(f)
 	defer file.Close()
 	scanner := bufio.NewScanner(file)
 	for scanner.Scan() {
 		if strings.Contains(scanner.Text(), pattern) {
-			result = append(result, scanner.Text())
+			line := scanner.Text()
+			if slices.Contains(flags, "-n") {
+				line = fmt.Sprintf("%d:%s", lineNum+1, line)
+			}
+			if slices.Contains(flags, "-l") && !slices.Contains(result, f) {
+				result = append(result, f)
+			} else {
+				result = append(result, line)
+			}
 		}
+		lineNum++
 	}
 	return result
 }
-
