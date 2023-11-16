8901c3c3e9313d2a7daf206bde382ef40e79840d
Jiří Martínek <jmartinek@corpus.cz>
2023-11-16 18:02:42 +0100

    go_pascals-triangle done.

--- go/pascals-triangle/pascals_triangle.go
+++ go/pascals-triangle/pascals_triangle.go
@@ -1,5 +1,18 @@
 package pascal
 
 func Triangle(n int) [][]int {
-	panic("Please implement the Triangle function")
+	result := make([][]int, n)
+	for i := 0; i < n; i++ {
+		result[i] = make([]int, i+1)
+	}
+	for i := range result {
+		for j := range result[i] {
+			if j == 0 || j == i {
+				result[i][j] = 1
+			} else {
+				result[i][j] = result[i - 1][j-1] + result[i -1][j]
+			}
+		}
+	}
+	return result
 }
