42d1d2ef38565f28120840a739cf80588f5f02fd
Jiří Martínek <jmartinek@corpus.cz>
2023-11-21 18:42:18 +0100

    go_yacht dice values done.

--- go/yacht/yacht.go
+++ go/yacht/yacht.go
@@ -1,5 +1,28 @@
 package yacht
 
+var categoryValues = map[string]int{
+	"ones":   1,
+	"twos":   2,
+	"threes": 3,
+	"fours":  4,
+	"fives":  5,
+	"sixes":  6,
+}
+
 func Score(dice []int, category string) int {
-	panic("Please implement the Score function")
+	categoryValue, ok := categoryValues[category]
+	if ok {
+		return dicesValue(dice, categoryValue)
+	}
+	return 0
+}
+
+func dicesValue(dice []int, categoryValue int) int {
+	var result int
+	for _, v := range dice {
+		if v == categoryValue {
+			result += v
+		}
+	}
+	return result
 }
