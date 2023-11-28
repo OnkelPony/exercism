84664897f959a2865b30d52f32cd58b11620a02a
Jiří Martínek <zkurvenej@outlook.com>
2023-11-28 22:37:23 +0000

    go_food-chain Verse() done.

--- go/food-chain/food_chain.go
+++ go/food-chain/food_chain.go
@@ -4,28 +4,32 @@ import "fmt"
 
 var firstVerse = "I know an old lady who swallowed a %s.\n"
 var lastVerse = "I don't know why she swallowed the fly. Perhaps she'll die."
-var uniSwallow = "She swallowed the %s to catch the %s."
-//var onlyBirdSwallow = "She swallowed the bird to catch the spider that wriggled and jiggled and tickled inside her."
+var uniSwallow = "She swallowed the %s to catch the %s%s.\n"
+var onlyBirdSwallow = ""
 var animals = []string{"fly", "spider", "bird", "cat", "dog", "goat", "cow", "horse"}
 var swallows = []string{
-	"It wriggled and jiggled and tickled inside her.",
-	"How absurd to swallow a bird!",
-	"Imagine that, to swallow a cat!",
-	"What a hog, to swallow a dog!",
-	"Just opened her throat and swallowed a goat!",
-	"I don't know how she swallowed a cow!",
+	"It wriggled and jiggled and tickled inside her.\n",
+	"How absurd to swallow a bird!\n",
+	"Imagine that, to swallow a cat!\n",
+	"What a hog, to swallow a dog!\n",
+	"Just opened her throat and swallowed a goat!\n",
+	"I don't know how she swallowed a cow!\n",
 }
 
 func Verse(v int) string {
-	result := fmt.Sprintf(firstVerse, v-1)
+	result := fmt.Sprintf(firstVerse, animals[v-1])
 	if v == 8 {
 		result += "She's dead, of course!"
 		return result
 	}
 	if v > 1 {
 		result += swallows[v-2]
-		for i := 0; i < v; i++ {
-			result += fmt.Sprintf(uniSwallow, animals[i+1], animals[i])
+		for i := v - 1; i > 0; i-- {
+			if i == 2 {
+				onlyBirdSwallow = " that wriggled and jiggled and tickled inside her"
+			}
+			result += fmt.Sprintf(uniSwallow, animals[i], animals[i-1], onlyBirdSwallow)
+			onlyBirdSwallow = ""
 		}
 	}
 	result += lastVerse
