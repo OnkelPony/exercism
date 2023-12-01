e6e56d3149ca717e7b22409246fa5b00df51294f
Jiří Martínek <zkurvenej@outlook.com>
2023-12-01 11:22:45 +0000

    go_house Verse() done.

--- go/house/house.go
+++ go/house/house.go
@@ -8,9 +8,17 @@ type SubjectDoing struct {
 }
 
 var verses = map[int]SubjectDoing{
-	2: {S: "malt", D: "lay"},
-	3: {S: "rat", D: "ate"},
-	4: {S: "cat", D: "killed"},
+	2:  {S: "malt", D: "lay"},
+	3:  {S: "rat", D: "ate"},
+	4:  {S: "cat", D: "killed"},
+	5:  {S: "dog", D: "worried"},
+	6:  {S: "cow with the crumpled horn", D: "tossed"},
+	7:  {S: "maiden all forlorn", D: "milked"},
+	8:  {S: "man all tattered and torn", D: "kissed"},
+	9:  {S: "priest all shaven and shorn", D: "married"},
+	10: {S: "rooster that crowed in the morn", D: "woke"},
+	11: {S: "farmer sowing his corn", D: "kept"},
+	12: {S: "horse and the hound and the horn", D: "belonged to"},
 }
 
 func Verse(v int) string {
