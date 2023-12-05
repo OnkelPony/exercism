55866e1135a09f699f32070d8097a62441b1eb01
Jiří Martínek <zkurvenej@outlook.com>
2023-12-05 21:30:16 +0100

    go_spiral-matrix empty matrix done.

--- go/spiral-matrix/spiral_matrix.go
+++ go/spiral-matrix/spiral_matrix.go
@@ -1,5 +1,11 @@
 package spiralmatrix
 
 func SpiralMatrix(size int) [][]int {
-	panic("Please implement the SpiralMatrix function")
+
+	result := make([][]int, size)
+	for i := 0; i < size; i++ {
+		result[i] = make([]int, size)
+	}
+	
+	return result
 }
