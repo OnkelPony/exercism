77bf69f58c0309125fbf9bf6c83247944fbb7991
Jiří Martínek <zkurvenej@outlook.com>
2023-12-06 19:58:22 +0100

    go_spiral-matrix done.

--- go/spiral-matrix/spiral_matrix.go
+++ go/spiral-matrix/spiral_matrix.go
@@ -6,6 +6,15 @@ func SpiralMatrix(size int) [][]int {
 	for i := 0; i < size; i++ {
 		result[i] = make([]int, size)
 	}
-	
+	col, row := 0, 0
+	dCol, dRow := 1, 0
+	for i := 1; i <= size*size; i++ {
+		result[row][col] = i
+		if col+dCol == size || row+dRow == size || col+dCol < 0 || result[row+dRow][col+dCol] > 0 {
+			dCol, dRow = -dRow, dCol
+		}
+		row += dRow
+		col += dCol
+	}
 	return result
 }
