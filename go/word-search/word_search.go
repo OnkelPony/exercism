be27933e35077d06c6af45bdf32af432ee989e31
Jiří Martínek <jmartinek@corpus.cz>
2023-12-07 19:04:05 +0100

    go_word-search skeleton done.

--- go/word-search/word_search.go
+++ go/word-search/word_search.go
@@ -1,6 +1,19 @@
 package wordsearch
 
+import "errors"
+
 func Solve(words []string, puzzle []string) (map[string][2][2]int, error) {
 	result := make(map[string][2][2]int)
-	
+	var err error
+	for _, word := range words {
+		result[word] = findWord(word, puzzle)
+		if result[word] == [2][2]int{{-1, -1}, {-1, -1}} {
+		err = errors.New("word not found")
+		}
+	}
+	return result, err
+}
+
+func findWord(word string, puzzle []string) [2][2]int{
+	return [2][2]int{{-1, -1}, {-1, -1}} 
 }
