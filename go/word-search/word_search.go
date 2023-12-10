81f165336d4e5b15095afe53e777177f1cf4a84c
Jiří Martínek <zkurvenej@outlook.com>
2023-12-10 13:28:35 +0100

    go_word-search skeleton++ done.

--- go/word-search/word_search.go
+++ go/word-search/word_search.go
@@ -17,6 +17,15 @@ func Solve(words []string, puzzle []string) (map[string][2][2]int, error) {
 func findWord(word string, puzzle []string) [2][2]int{
 	width := len(puzzle[0])
 	height := len(puzzle)
-	
+	initialPositions := [][2]int{}
+	for i := 0; i < height; i++ {
+		for j := 0; j < width; j++ {
+			if puzzle[i][j] == word[0] {
+				initialPositions = append(initialPositions, [2]int{i, j})
+			}
+		}
+	}
+	for _, position := range initialPositions {
+		
 	return [2][2]int{{-1, -1}, {-1, -1}} 
 }
