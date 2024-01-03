0d5d6c613cce3e2c52a6795613b1ebd9d5062c11
Jiří Martínek <jmartinek@corpus.cz>
2024-01-03 19:00:32 +0100

    go_minesweeper empty field solved.

--- go/minesweeper/minesweeper.go
+++ go/minesweeper/minesweeper.go
@@ -1,6 +1,18 @@
 package minesweeper
 
+import (
+	"bytes"
+)
+
 // Annotate returns an annotated board
 func Annotate(board []string) []string {
-	panic("Please implement the Annotate function")
+	result := make([]string, len(board))
+	for row := range board {
+		var buf bytes.Buffer
+		for col := range board[row] {
+			buf.WriteByte(board[row][col])
+		}
+		result[row] = buf.String()
+	}
+	return result
 }
