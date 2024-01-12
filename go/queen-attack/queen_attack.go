9100172b5d622e197bccc49469df073fe3f519cb
Jiří Martínek <jmartinek@corpus.cz>
2024-01-12 18:09:43 +0100

    go_queen-attack invalid positions check started.

--- go/queen-attack/queen_attack.go
+++ go/queen-attack/queen_attack.go
@@ -1,5 +1,21 @@
 package queenattack
 
+import "fmt"
+
 func CanQueenAttack(whitePosition, blackPosition string) (bool, error) {
-	panic("Please implement the CanQueenAttack function")
+	err := testValid(whitePosition, blackPosition)
+	if err != nil {
+		return false, err
+	}
+	return true, nil
+}
+
+func testValid(whitePosition, blackPosition string) error {
+	if whitePosition[0] < 'a' || whitePosition[0] > 'h' || whitePosition[1] < '1' || whitePosition[1] > '8' {
+		return fmt.Errorf("white position invalid")
+	}
+	if blackPosition[0] < 'a' || blackPosition[0] > 'h' || blackPosition[1] < '1' || blackPosition[1] > '8' {
+		return fmt.Errorf("black position invalid")
+	}
+	return nil
 }
