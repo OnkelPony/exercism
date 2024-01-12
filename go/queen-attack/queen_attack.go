28185e66b1ad431a211f77daa3f204e13ecc2c5c
OnkelPony <zkurvenej@outlook.com>
2024-01-12 21:43:02 +0100

    go_queen-attack invalid positions done.

--- go/queen-attack/queen_attack.go
+++ go/queen-attack/queen_attack.go
@@ -11,11 +11,17 @@ func CanQueenAttack(whitePosition, blackPosition string) (bool, error) {
 }
 
 func testValid(whitePosition, blackPosition string) error {
+	if blackPosition == "" || whitePosition == "" {
+		return fmt.Errorf("position can't be empty")
+	}
 	if whitePosition[0] < 'a' || whitePosition[0] > 'h' || whitePosition[1] < '1' || whitePosition[1] > '8' {
 		return fmt.Errorf("white position invalid")
 	}
 	if blackPosition[0] < 'a' || blackPosition[0] > 'h' || blackPosition[1] < '1' || blackPosition[1] > '8' {
 		return fmt.Errorf("black position invalid")
 	}
+	if blackPosition == whitePosition {
+		return fmt.Errorf("white and black position can't be the same")
+    }
 	return nil
 }
