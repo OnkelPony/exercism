9823c61ebaef7f4006f2b49632faa9ed23fb1f05
Jiří Martínek <jmartinek@corpus.cz>
2024-01-18 19:07:54 +0100

    go_word-search refactored.

--- go/word-search/word_search.go
+++ go/word-search/word_search.go
@@ -2,9 +2,8 @@ package wordsearch
 
 import "errors"
 
-type Position [2]int
 
-var invalidPosition = Position{-1, -1}
+var invalidPosition = [2]int{-1, -1}
 
 func Solve(words []string, puzzle []string) (map[string][2][2]int, error) {
 	result := make(map[string][2][2]int)
@@ -13,38 +12,39 @@ func Solve(words []string, puzzle []string) (map[string][2][2]int, error) {
 		result[word] = findWord(word, puzzle)
 		if result[word] == [2][2]int{invalidPosition, invalidPosition} {
 			err = errors.New("word not found")
+			return result, err
 		}
 	}
-	return result, err
+	return result, nil
 }
 
 func findWord(word string, puzzle []string) [2][2]int {
 	for y := 0; y < len(puzzle); y++ {
 		for x := 0; x < len(puzzle[0]); x++ {
-			endPosition := scanDirections(word, puzzle, Position{x, y})
+			endPosition := scanDirections(word, puzzle, [2]int{x, y})
 			if endPosition != invalidPosition {
-				return [2][2]int{Position{x, y}, {endPosition[0], endPosition[1]}}
+				return [2][2]int{{x, y}, endPosition}
 			}
 		}
 	}
 	return [2][2]int{invalidPosition, invalidPosition}
 }
 
-func scanDirections(word string, puzzle []string, position Position) Position {
+func scanDirections(word string, puzzle []string, position [2]int) [2]int {
 	if word[0] != puzzle[position[1]][position[0]] {
 		return invalidPosition
 	}
 	var x, y int
-	directions := []Position{{0, 1}, {-1, 1}, {-1, 0}, {-1, -1}, {0, -1}, {1, -1}, {1, 0}, {1, 1}}
+	directions := [][2]int{{0, 1}, {-1, 1}, {-1, 0}, {-1, -1}, {0, -1}, {1, -1}, {1, 0}, {1, 1}}
 	for _, direction := range directions {
-		for i, v := range word {
+		for i, _ := range word {
 			x = position[0] + i*direction[0]
 			y = position[1] + i*direction[1]
-			if x < 0 || y < 0 || x > len(puzzle[0])-1 || y > len(puzzle)-1 || v != rune(puzzle[y][x]) {
+			if x < 0 || y < 0 || x >= len(puzzle[0]) || y >= len(puzzle) || word[i] != puzzle[y][x] {
 				break
 			}
 			if i == len(word)-1 {
-				return Position{x, y}
+				return [2]int{x, y}
 			}
 		}
 	}
