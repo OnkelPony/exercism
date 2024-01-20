0fa454113578f81e6ebd28eb56f6879fcbbdc314
Jiří Martínek <zkurvenej@outlook.com>
2024-01-20 19:17:59 +0100

    go_saddle-points skeleton done.

--- go/saddle-points/saddle_points.go
+++ go/saddle-points/saddle_points.go
@@ -1,11 +1,26 @@
 package matrix
 
-// Define the Matrix and Pair types here.
+import "slices"
 
-func New(s string) (*Matrix, error) {
-	panic("Please implement the New function")
-}
+// Define the Matrix and Pair types here.
+type Pair [2]int
 
 func (m *Matrix) Saddle() []Pair {
-	panic("Please implement the Saddle function")
+var result []Pair
+rowMaxes := getRowMaxes(m)
+colMins := getColMins(m)
+for _, rowMax := range rowMaxes {
+	if slices.Contains(colMins, rowMax) {
+		result = append(result, rowMax)
+	}
+}
+return result
+}
+
+func getColMins(m *Matrix) []Pair{
+	panic("unimplemented")
+}
+
+func getRowMaxes(m *Matrix) []Pair{
+	panic("unimplemented")
 }
