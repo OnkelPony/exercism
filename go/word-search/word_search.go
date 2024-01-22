fe1244cf537748222c5fbf088369541c0a298838
Jiří Martínek <jmartinek@corpus.cz>
2024-01-22 18:28:38 +0100

    go_word-search mentor refactored ;-)

--- go/word-search/word_search.go
+++ go/word-search/word_search.go
@@ -2,51 +2,55 @@ package wordsearch
 
 import "errors"
 
+var directions = [][2]int{{0, 1}, {-1, 1}, {-1, 0}, {-1, -1}, {0, -1}, {1, -1}, {1, 0}, {1, 1}}
 
-var invalidPosition = [2]int{-1, -1}
+var ErrWordNotFound = errors.New("word not found")
 
+// Solve should have a comment
 func Solve(words []string, puzzle []string) (map[string][2][2]int, error) {
-	result := make(map[string][2][2]int)
+	result := make(map[string][2][2]int, len(words))
 	var err error
 	for _, word := range words {
-		result[word] = findWord(word, puzzle)
-		if result[word] == [2][2]int{invalidPosition, invalidPosition} {
-			err = errors.New("word not found")
-			return result, err
+		if result[word], err = findWord(word, puzzle); err != nil {
+			return nil, err
 		}
 	}
 	return result, nil
 }
 
-func findWord(word string, puzzle []string) [2][2]int {
-	for y := 0; y < len(puzzle); y++ {
-		for x := 0; x < len(puzzle[0]); x++ {
-			endPosition := scanDirections(word, puzzle, [2]int{x, y})
-			if endPosition != invalidPosition {
-				return [2][2]int{{x, y}, endPosition}
+func findWord(word string, puzzle []string) ([2][2]int, error) {
+	for row := 0; row < len(puzzle); row++ {
+		for col := 0; col < len(puzzle[0]); col++ {
+			if endPosition, err := scanDirections(word, puzzle, col, row); err == nil {
+				return [2][2]int{{col, row}, endPosition}, nil
 			}
 		}
 	}
-	return [2][2]int{invalidPosition, invalidPosition}
+	return [2][2]int{}, ErrWordNotFound
 }
 
-func scanDirections(word string, puzzle []string, position [2]int) [2]int {
-	if word[0] != puzzle[position[1]][position[0]] {
-		return invalidPosition
+func scanDirections(word string, puzzle []string, startCol, startRow int) ([2]int, error) {
+	if word[0] != puzzle[startRow][startCol] {
+		return [2]int{}, ErrWordNotFound
 	}
-	var x, y int
-	directions := [][2]int{{0, 1}, {-1, 1}, {-1, 0}, {-1, -1}, {0, -1}, {1, -1}, {1, 0}, {1, 1}}
+
+	var col, row int
+direction:
 	for _, direction := range directions {
-		for i, _ := range word {
-			x = position[0] + i*direction[0]
-			y = position[1] + i*direction[1]
-			if x < 0 || y < 0 || x >= len(puzzle[0]) || y >= len(puzzle) || word[i] != puzzle[y][x] {
-				break
-			}
-			if i == len(word)-1 {
-				return [2]int{x, y}
+		distanceToEndOfWord := len(word) - 1
+		endCol := startCol + direction[0]*distanceToEndOfWord
+		endRow := startRow + direction[1]*distanceToEndOfWord
+		if endCol < 0 || endCol >= len(puzzle[0]) || endRow < 0 || endRow >= len(puzzle) {
+			continue
+		}
+		for i := range word {
+			col = startCol + i*direction[0]
+			row = startRow + i*direction[1]
+			if word[i] != puzzle[row][col] {
+				continue direction
 			}
 		}
+		return [2]int{col, row}, nil
 	}
-	return invalidPosition
+	return [2]int{}, ErrWordNotFound
 }
