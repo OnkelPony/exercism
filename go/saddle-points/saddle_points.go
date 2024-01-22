78180276147df9b492d67c3eed339544794d4f92
Jiří Martínek <zkurvenej@outlook.com>
2024-01-22 22:21:45 +0100

    go_saddle-points skeleton broken.

--- go/saddle-points/saddle_points.go
+++ go/saddle-points/saddle_points.go
@@ -6,21 +6,41 @@ import "slices"
 type Pair [2]int
 
 func (m *Matrix) Saddle() []Pair {
-var result []Pair
-rowMaxes := getRowMaxes(m)
-colMins := getColMins(m)
-for _, rowMax := range rowMaxes {
-	if slices.Contains(colMins, rowMax) {
-		result = append(result, rowMax)
+	var result []Pair
+	rowMaxes := getRowMaxes(m)
+	colMins := getColMins(m)
+	for _, rowMax := range rowMaxes {
+		if slices.Contains(colMins, rowMax) {
+			result = append(result, rowMax)
+		}
 	}
-}
-return result
-}
-
-func getColMins(m *Matrix) []Pair{
-	panic("unimplemented")
+	return result
 }
 
-func getRowMaxes(m *Matrix) []Pair{
-	panic("unimplemented")
+func getColMins(m *Matrix) []Pair {
+	var result []Pair
+	cols := m.Cols()
+	for i := range cols {
+		minValue := cols[i][0]
+		for j := range cols[i] {
+			if cols[i][j] < minValue {
+				result[i] = Pair{i, j}
+			}
+		}
+	}
+	return result
+}
+
+func getRowMaxes(m *Matrix) []Pair {
+	var result []Pair
+	rows := m.Rows()
+	for j := range rows {
+		maxValue := rows[j][0]
+		for i := range rows[j] {
+			if rows[j][i] > maxValue {
+				result[j] = Pair{i, j}
+			}
+		}
+	}
+	return result
 }
