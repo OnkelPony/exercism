2dc6cc4f24cb160ebe40e27894be0049f2e05eca
Jiří Martínek <jmartinek@corpus.cz>
2024-01-26 18:58:08 +0100

    go_transpose empty string done.

--- go/transpose/transpose.go
+++ go/transpose/transpose.go
@@ -1,5 +1,17 @@
 package transpose
 
 func Transpose(input []string) []string {
-	panic("Please implement the Transpose function")
+	if len(input) == 0 {
+		return input
+	}
+	h := 0 
+	// w := len(input)
+	for _, row := range input {
+		if len(row) > h {
+			h = len(row)
+		}
+	}
+	result := make([]string, h)
+	return result
+
 }
