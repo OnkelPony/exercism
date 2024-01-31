29d3db742fa984b950de49683543ad6de775b073
Jiří Martínek <jmartinek@corpus.cz>
2024-01-31 19:32:19 +0100

    go_circular-buffer read and write done.

--- go/circular-buffer/circular_buffer.go
+++ go/circular-buffer/circular_buffer.go
@@ -1,5 +1,7 @@
 package circular
 
+import "errors"
+
 // Implement a circular buffer of bytes supporting both overflow-checked writes
 // and unconditional, possibly overwriting, writes.
 //
@@ -8,17 +10,40 @@ package circular
 // replacement for anything using that interface.
 
 // Define the Buffer type here.
+type Buffer struct {
+	items   []byte
+	toRead  int
+	toWrite int
+	used int
+}
 
 func NewBuffer(size int) *Buffer {
-	panic("Please implement the NewBuffer function")
+	result := new(Buffer)
+	result.items = make([]byte, size)
+	result.toRead = 0
+	result.toWrite = 0
+	result.used = 0
+	return result
 }
 
 func (b *Buffer) ReadByte() (byte, error) {
-	panic("Please implement the ReadByte function")
+	if b.used == 0{
+		return 0, errors.New("buffer is empty")
+	}
+	result := b.items[b.toRead]
+	b.used--
+	b.toRead = (b.toRead + 1) % len(b.items)
+	return result, nil
 }
 
 func (b *Buffer) WriteByte(c byte) error {
-	panic("Please implement the WriteByte function")
+	if b.used == len(b.items) {
+		return errors.New("buffer is empty")
+	}
+	b.items[b.toWrite] = c
+	b.used++
+	b.toWrite = (b.toWrite + 1) % len(b.items)
+	return nil
 }
 
 func (b *Buffer) Overwrite(c byte) {
