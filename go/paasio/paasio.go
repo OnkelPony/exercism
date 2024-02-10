23b7728be3adbc9eca9a5a3a95ba025d2d75c436
Jiří Martínek <zkurvenej@outlook.com>
2024-02-10 17:12:46 +0100

    go_paasio not very consistent results.

--- go/paasio/paasio.go
+++ go/paasio/paasio.go
@@ -1,65 +1,97 @@
 package paasio
 
-import "io"
+import (
+	"io"
+	"sync"
+)
 
 // Define readCounter and writeCounter types here.
 type readCounter struct {
 	ioReader io.Reader
+	n        int64
+	nops     int
+	mu       sync.Mutex
 }
 
 type writeCounter struct {
 	ioWriter io.Writer
+	n        int64
+	nops     int
+	mu       sync.Mutex
 }
 
 // For the return of the function NewReadWriteCounter, you must also define a type that satisfies the ReadWriteCounter interface.
 type readWriteCounter struct {
 	ioReadWriter io.ReadWriter
+	n            int64
+	nops         int
+	mu           sync.Mutex
 }
 
 func NewWriteCounter(writer io.Writer) WriteCounter {
-	return &writeCounter{writer}
+	return &writeCounter{ioWriter: writer}
 }
 
 func NewReadCounter(reader io.Reader) ReadCounter {
-	return &readCounter{reader}
+	return &readCounter{ioReader: reader}
 }
 
 func NewReadWriteCounter(readwriter io.ReadWriter) ReadWriteCounter {
-	return &readWriteCounter{readwriter}
+	return &readWriteCounter{ioReadWriter: readwriter}
 }
 
 func (rc *readCounter) Read(p []byte) (int, error) {
-	return rc.ioReader.Read(p)
+	rc.mu.Lock()
+	defer rc.mu.Unlock()
+	n, err := rc.ioReader.Read(p)
+	rc.n += int64(n)
+	rc.nops++
+	return n, err
 }
 
 func (rc *readCounter) ReadCount() (int64, int) {
-	panic("Please implement the ReadCount function")
+	return rc.n, rc.nops
 }
 
 func (wc *writeCounter) Write(p []byte) (int, error) {
-	return wc.ioWriter.Write(p)
+	wc.mu.Lock()
+	defer wc.mu.Unlock()
+	n, err := wc.ioWriter.Write(p)
+	wc.n += int64(n)
+	wc.nops++
+	return n, err
 }
 
 func (wc *writeCounter) WriteCount() (int64, int) {
-	panic("Please implement the WriteCount function")
+	return wc.n, wc.nops
 }
 
 // Read implements ReadWriteCounter.
-func (rwc *readWriteCounter) Read(p []byte) (n int, err error) {
-	return rwc.ioReadWriter.Read(p)
+func (rwc *readWriteCounter) Read(p []byte) (int, error) {
+	rwc.mu.Lock()
+	defer rwc.mu.Unlock()
+	n, err := rwc.ioReadWriter.Read(p)
+	rwc.n += int64(n)
+	rwc.nops++
+	return n, err
 }
 
 // ReadCount implements ReadWriteCounter.
-func (rwc *readWriteCounter) ReadCount() (n int64, nops int) {
-	panic("unimplemented")
+func (rwc *readWriteCounter) ReadCount() (int64, int) {
+	return rwc.n, rwc.nops
 }
 
 // Write implements ReadWriteCounter.
-func (rwc *readWriteCounter) Write(p []byte) (n int, err error) {
-	return rwc.ioReadWriter.Write(p)
+func (rwc *readWriteCounter) Write(p []byte) (int, error) {
+	rwc.mu.Lock()
+	defer rwc.mu.Unlock()
+	n, err := rwc.ioReadWriter.Write(p)
+	rwc.n += int64(n)
+	rwc.nops++
+	return n, err
 }
 
 // WriteCount implements ReadWriteCounter.
-func (rwc *readWriteCounter) WriteCount() (n int64, nops int) {
-	panic("unimplemented")
+func (rwc *readWriteCounter) WriteCount() (int64, int) {
+	return rwc.n, rwc.nops
 }
