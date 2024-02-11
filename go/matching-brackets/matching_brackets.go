2909956c1a2fdaaa0bf1e129fd08a8442a543766
Jiří Martínek <zkurvenej@outlook.com>
2024-02-11 16:13:52 +0100

    go_matching-brackets paired square brackets done.

--- go/matching-brackets/matching_brackets.go
+++ go/matching-brackets/matching_brackets.go
@@ -1,5 +1,27 @@
 package brackets
 
+import "slices"
+
+var openBrackets = []byte{'(', '[', '{'}
+var closeBrackets = []byte{')', ']', '}'}
+var bracketPairs = map[byte]byte{
+	'(': ')',
+	'[': ']',
+	'{': '}',
+}
+
 func Bracket(input string) bool {
-	panic("Please implement the Bracket function")
+	brackets := []byte{}
+	for i := 0; i < len(input); i++ {
+		if slices.Contains(openBrackets, input[i]) {
+			brackets = append(brackets, input[i])
+		} else if slices.Contains(closeBrackets, input[i]) {
+			if bracketPairs[brackets[len(brackets)-1]] == input[i] {
+				brackets = brackets[:len(brackets)-1]
+			} else {
+				return false
+			}
+		}
+	}
+	return len(brackets) == 0
 }
