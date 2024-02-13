f2cbcd0081ef332ffc82837d0898ce2378f9982e
Jiří Martínek <jmartinek@corpus.cz>
2024-02-13 18:41:18 +0100

    go_matching-brackets done.

--- go/matching-brackets/matching_brackets.go
+++ go/matching-brackets/matching_brackets.go
@@ -1,27 +1,31 @@
 package brackets
 
-import "slices"
-
-var openBrackets = []byte{'(', '[', '{'}
-var closeBrackets = []byte{')', ']', '}'}
-var bracketPairs = map[byte]byte{
-	'(': ')',
-	'[': ']',
-	'{': '}',
-}
-
 func Bracket(input string) bool {
-	brackets := []byte{}
+	var stack []byte
 	for i := 0; i < len(input); i++ {
-		if slices.Contains(openBrackets, input[i]) {
-			brackets = append(brackets, input[i])
-		} else if slices.Contains(closeBrackets, input[i]) {
-			if bracketPairs[brackets[len(brackets)-1]] == input[i] {
-				brackets = brackets[:len(brackets)-1]
-			} else {
+		char := input[i] 
+		switch char {
+		case '(', '[', '{':
+			stack = append(stack, char)
+		case ')', ']', '}':
+			if len(stack) == 0 || stack[len(stack)-1] != matchingBracket(int(char)) {
 				return false
 			}
+			stack = stack[:len(stack)-1]
 		}
 	}
-	return len(brackets) == 0
+	return len(stack) == 0
+}
+
+func matchingBracket(char int) byte {
+	switch char {
+	case ')':
+		return '('
+	case ']':
+		return '['
+	case '}':
+		return '{'
+	default:
+		return 0
+	}
 }
