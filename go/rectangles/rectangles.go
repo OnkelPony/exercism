10a3d2fd85e48c1d5a1e5b20d9a99278dcea30e9
Jiří Martínek <jmartinek@corpus.cz>
2024-02-15 18:55:35 +0100

    go_rectangles sides check failing.

--- go/rectangles/rectangles.go
+++ go/rectangles/rectangles.go
@@ -2,10 +2,22 @@ package rectangles
 
 func Count(diagram []string) int {
 	var result int
-	for h := 0; h < len(diagram); h++ {
-		for w := 0; w < len(diagram[h]); w++ {
+	for h := 0; h < len(diagram) - 1; h++ {
+		for w := 0; w < len(diagram[h]) - 1; w++ {
 			if diagram[h][w] == '+' {
-				result += 1
+				result += countRectangles(diagram, h, w)
+			}
+		}
+	}
+	return result
+}
+
+func countRectangles(diagram []string, h, w int) int {
+	var result int
+	for y := h + 1; y < len(diagram); y++ {
+		for x := w + 1; x < len(diagram[h]); x++ {
+			if diagram[y][x] == '+' && diagram[y][w] == '+' && diagram[h][x] == '+' {
+				result++
 			}
 		}
 	}
