eaba8639430b8d4aca565c1b081dd0cb223352f6
Jiří Martínek <jmartinek@corpus.cz>
2024-02-15 19:24:52 +0100

    go_rectangles implement sidesExist().

--- go/rectangles/rectangles.go
+++ go/rectangles/rectangles.go
@@ -1,25 +1,42 @@
 package rectangles
 
+type Point struct {
+	x int
+	y int
+}
+
 func Count(diagram []string) int {
 	var result int
-	for h := 0; h < len(diagram) - 1; h++ {
-		for w := 0; w < len(diagram[h]) - 1; w++ {
+	for h := 0; h < len(diagram)-1; h++ {
+		for w := 0; w < len(diagram[h])-1; w++ {
 			if diagram[h][w] == '+' {
-				result += countRectangles(diagram, h, w)
+				result += countRectangles(diagram, Point{w, h})
 			}
 		}
 	}
 	return result
 }
 
-func countRectangles(diagram []string, h, w int) int {
+func countRectangles(diagram []string, topLeft Point) int {
 	var result int
-	for y := h + 1; y < len(diagram); y++ {
-		for x := w + 1; x < len(diagram[h]); x++ {
-			if diagram[y][x] == '+' && diagram[y][w] == '+' && diagram[h][x] == '+' {
+	var botRight Point
+	for botRight.y = topLeft.y + 1; botRight.y < len(diagram); botRight.y++ {
+		for botRight.x = topLeft.x + 1; botRight.x < len(diagram[topLeft.y]); botRight.x++ {
+			if diagram[botRight.y][botRight.x] == '+' &&
+				diagram[botRight.y][topLeft.x] == '+' &&
+				diagram[topLeft.y][botRight.x] == '+' &&
+				sidesExist(diagram, topLeft, botRight) {
 				result++
 			}
 		}
 	}
 	return result
+}
+
+func sidesExist(diagram []string, topLeft, botRight Point) bool{
+for x := topLeft.x + 1; x < botRight.x; x++ {
+	if diagram[topLeft.y][x] != '+' || diagram[topLeft.y][x] != '-' {
+		return false
+	}
+	
 }
