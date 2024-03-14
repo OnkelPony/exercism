3df73d4151f7b81c33f5224b72ff675d9c347514
Jiří Martínek <jmartinek@corpus.cz>
2024-03-14 19:03:47 +0100

    go_dominoes naive approach using sort.Slice.

--- go/dominoes/dominoes.go
+++ go/dominoes/dominoes.go
@@ -1,9 +1,13 @@
 package dominoes
 
+import "sort"
+
 // Define the Domino type here.
 type Domino [2]int
 
 func MakeChain(input []Domino) ([]Domino, bool) {
-	
+	sort.Slice(input, func(i, j int) bool {
+		return input[i][1] == input[j][1] || input[i][1] == input[j][0]
+	})
 	return input, true
 }
