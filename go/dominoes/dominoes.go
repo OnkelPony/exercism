b43c6a91aa6543b4dc54f4be2876bf738cedba36
Jiří Martínek <jmartinek@corpus.cz>
2024-03-14 18:49:24 +0100

    go_dominoes tests failing.

--- go/dominoes/dominoes.go
+++ go/dominoes/dominoes.go
@@ -1,7 +1,9 @@
 package dominoes
 
 // Define the Domino type here.
+type Domino [2]int
 
 func MakeChain(input []Domino) ([]Domino, bool) {
-	panic("Please implement the MakeChain function")
+	
+	return input, true
 }
