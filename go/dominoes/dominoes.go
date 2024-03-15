273d1fa45797fa50f758bd181687241265f50fb4
Jiří Martínek <jmartinek@corpus.cz>
2024-03-15 18:28:24 +0100

    go_dominoes skeleton done.

--- go/dominoes/dominoes.go
+++ go/dominoes/dominoes.go
@@ -1,13 +1,23 @@
 package dominoes
 
-import "sort"
 
 // Define the Domino type here.
 type Domino [2]int
 
 func MakeChain(input []Domino) ([]Domino, bool) {
-	sort.Slice(input, func(i, j int) bool {
-		return input[i][1] == input[j][1] || input[i][1] == input[j][0]
-	})
-	return input, true
+	if len(input) == 0 || len(input) == 1 && input[0][0] == input[0][1]{
+		return input, true
+	}
+	result := make([]Domino, 0, len(input))
+	result[0] = input[0]
+	//Stone con't be key of the map, stones can be non-unique
+	unusedStones := makeUnusedStones(input)
+	for i := 1; i < len(input); i++ {
+		nextStone, err := pickNext(unusedStones, result[i - 1])
+		if err != nil {
+			return nil, false
+		}
+		result[i] = nextStone
+	}
+	return result, true
 }
