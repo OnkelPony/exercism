b3a1ab0c5fd8325ee70ee2319917cf813badd7d3
OnkelPony <zkurvenej@outlook.com>
2024-03-17 10:48:16 +0100

    go_dominos compilation runs.

--- go/dominoes/dominoes.go
+++ go/dominoes/dominoes.go
@@ -7,7 +7,10 @@ func MakeChain(input []Domino) ([]Domino, bool) {
 	if len(input) == 0 || len(input) == 1 && input[0][0] == input[0][1] {
 		return input, true
 	}
-	result := make([]Domino, 0, len(input))
+	if len(input) == 1 {
+		return nil, false
+	}
+	result := make([]Domino, len(input))
 	result[0] = input[0]
 	//Stone con't be key of the map, stones can be non-unique
 	unusedStones := makeUnusedStones(input)
@@ -21,10 +24,14 @@ func MakeChain(input []Domino) ([]Domino, bool) {
 	return result, true
 }
 
+func pickNext(unusedStones map[int]Domino, domino Domino) (Domino, error) {
+	return domino, nil
+}
+
 func makeUnusedStones(input []Domino) map[int]Domino {
 	result := make(map[int]Domino, len(input)-1)
 	for i := 1; i < len(input); i++ {
-		result[i - 1] = input[i]
+		result[i-1] = input[i]
 	}
 	return result
 }
