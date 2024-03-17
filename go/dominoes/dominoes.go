7cdcf0bd824876b6864089544ba4fc53c063d1e0
OnkelPony <zkurvenej@outlook.com>
2024-03-17 10:18:07 +0100

    go_dominos makeUnusedStones() done.

--- go/dominoes/dominoes.go
+++ go/dominoes/dominoes.go
@@ -1,11 +1,10 @@
 package dominoes
 
-
 // Define the Domino type here.
 type Domino [2]int
 
 func MakeChain(input []Domino) ([]Domino, bool) {
-	if len(input) == 0 || len(input) == 1 && input[0][0] == input[0][1]{
+	if len(input) == 0 || len(input) == 1 && input[0][0] == input[0][1] {
 		return input, true
 	}
 	result := make([]Domino, 0, len(input))
@@ -13,11 +12,19 @@ func MakeChain(input []Domino) ([]Domino, bool) {
 	//Stone con't be key of the map, stones can be non-unique
 	unusedStones := makeUnusedStones(input)
 	for i := 1; i < len(input); i++ {
-		nextStone, err := pickNext(unusedStones, result[i - 1])
+		nextStone, err := pickNext(unusedStones, result[i-1])
 		if err != nil {
 			return nil, false
 		}
 		result[i] = nextStone
 	}
 	return result, true
+}
+
+func makeUnusedStones(input []Domino) map[int]Domino {
+	result := make(map[int]Domino, len(input)-1)
+	for i := 1; i < len(input); i++ {
+		result[i - 1] = input[i]
+	}
+	return result
 }
